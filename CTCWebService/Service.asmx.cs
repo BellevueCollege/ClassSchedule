0748f07a2160ec44655fbd500458a395a80dfdd7
Jeremy Johnson <jjohnson@pencol.edu>
2011-05-18 14:56:35 -0700

    Added REST WebService, GetFootnotes implemented

---
+++ CTCWebService/Service.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Services;
+using System.Web;
+using System.Web.Services;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Ctc.Ods.Data;
+using Ctc.Ods.Types;
+
+namespace Ctc.WebService
+{
+	/// <summary>
+	/// Summary description for Service1
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+	// [System.Web.Script.Services.ScriptService]
+
+	public class Service : System.Web.Services.WebService
+	{
+
+
+		[WebMethod]
+		public List<Footnote> GetFootnotes(string id, string id2 = null)
+		{
+			OdsRepository repo = new OdsRepository();
+			return repo.GetFootnote(id, id2).ToList();
+		}
+	}
+}
