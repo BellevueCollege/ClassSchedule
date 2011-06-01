d12655bf67b4a4f2bbae217a7bc8373c66a844d8
Shawn South <shawn.south@bellevuecollege.edu>
2011-05-31 18:38:49 -0700

    Integrated days of the week.

--- Research/Bellevue-SectionInstructorTimeAndLocation.sql
+++ Research/Bellevue-SectionInstructorTimeAndLocation.sql
@@ -9,35 +9,32 @@ select distinct *
 from (
 	select
 		c.ClassID
-		,c.DayID
+		,d.Title
 		,c.StartTime
 		,c.EndTime
 		,c.InstructorSID
 		,c.InstructorName
 		,c.Room
 	from vw_Class c
-	where not c.Room is null
-	and not c.StartTime is null
-	and not c.EndTime is null
-	and not c.DayID is null
-	and c.SectionStatusID1 not like '%Z%'
+	join vw_Day d on c.DayID = d.DayID
+	where c.SectionStatusID1 not like '%Z%'
 	and c.SectionStatusID1 not like '%X%'
-	and right(c.ClassID, 4) = 'B014'
 	union
 	select
 	--	i.InstructionID
 		i.ClassID
-		,i.DayID
+		,d.Title
 		,i.StartTime
 		,i.EndTime
 		,i.InstructorSID
 		,i.InstructorName
 		,i.Room
 	from vw_Instruction i
+	join vw_Day d on i.DayID = d.DayID
 	where not i.Room is null
 	and not i.StartTime is null
 	and not i.EndTime is null
 	and not i.DayID is null
-	and right(i.ClassID, 4) = 'B014'
 	--order by right(i.ClassID, 4) desc
 ) a
+where RIGHT(ClassID, 4) = 'B014'
