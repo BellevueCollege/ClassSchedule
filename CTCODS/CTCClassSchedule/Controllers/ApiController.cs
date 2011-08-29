70d68edf1ad9c51f391198427ca18d79cce68643
Shawn South <shawn.south@bellevuecollege.edu>
2011-08-29 13:06:16 -0700

    Added new API controller and initial view for
    returning list of subjects.

---
+++ CTCODS/CTCClassSchedule/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace CTCClassSchedule.Controllers
+{
+    public class ApiController : Controller
+    {
+        //
+        // GET: /Api/
+
+        public ActionResult Subjects()
+        {
+            return View();
+        }
+
+    }
+}
