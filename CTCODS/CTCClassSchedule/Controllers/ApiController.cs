0b807a4061b367767cda140e126f5826a54f4bfc
Shawn South <shawn.south@bellevuecollege.edu>
2011-08-30 18:34:08 -0700

    Implemented returning Course subjects as a JSON
    array.

--- CTCODS/CTCClassSchedule/Controllers/ApiController.cs
+++ CTCODS/CTCClassSchedule/Controllers/ApiController.cs
@@ -8,13 +8,34 @@ namespace CTCClassSchedule.Controllers
 	public class ApiController : Controller
 	{
 		//
-		// GET: /Api/
+		// GET: /Api/Subjects
+		/// <summary>
+		/// Returns an array of <see cref="Course"/> Subjects
+		/// </summary>
+		/// <param name="format"></param>
+		/// <returns>
+		///		Either a <see cref="PartialViewResult"/> which can be embedded in an MVC View,
+		///		or the list of <see cref="Course"/> Subjects as a JSON array.
+		/// </returns>
+		/// <remarks>
+		///		To receive the list as a JSON array call this method with <i>format=json</i>:
+		///		<example>
+		///			http://localhost/Api/Subjects?format=json
+		///		</example>
+		/// </remarks>
 		[HttpGet]
-		public ActionResult Subjects()
+		public ActionResult Subjects(string format)
 		{
 			using (OdsRepository db = new OdsRepository())
 			{
 				IList<CoursePrefix> data = db.GetCourseSubjects();
+
+				if (format == "json")
+				{
+					// NOTE: AllowGet exposes a potential for JSON Hijacking (http://haacked.com/archive/2009/06/25/json-hijacking.aspx)
+					// but is not an issue here because we are returning public (e.g. non-sensitive) data
+					return Json(data, JsonRequestBehavior.AllowGet);
+				}
 
 				ViewBag.SubjectsColumns = 2;
 				return PartialView(data);
