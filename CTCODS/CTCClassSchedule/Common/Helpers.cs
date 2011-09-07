20d1d7e4687ecc27acf0870fb632d97618a31093
Shawn South <shawn.south@bellevuecollege.edu>
2011-09-07 15:37:52 -0700

    Made several methods static, and moved to helper
    class where possible.

--- CTCODS/CTCClassSchedule/Common/Helpers.cs
+++ CTCODS/CTCClassSchedule/Common/Helpers.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Html;
 using System.Net;
-using System.Security.Cryptography;
-
-
+using System.Web.Mvc;
+using Ctc.Ods;
 
 namespace CTCClassSchedule.Common
 {
@@ -30,10 +25,306 @@ namespace CTCClassSchedule.Common
 
 		}
 
+		/// <summary>
+		/// returns an IList<ISectionFacet> that contains all of the facet information
+		/// passed into the app by the user clicking on the faceted search left pane
+		/// facets accepted: flex, time, days, availability
+		/// </summary>
+		static public IList<ISectionFacet> addFacets(string timestart, string timeend, string day_su, string day_m, string day_t, string day_w, string day_th, string day_f, string day_s, string f_oncampus, string f_online, string f_hybrid, string f_telecourse, string avail)
+		{
+			IList<ISectionFacet> facets = new List<ISectionFacet>();
+
+			// TODO: can we group facets? e.g. "classformat=online / =hybrid / etc. Would be easier to programmatically evaluate (could use select/case).
+			//add the class format facet options (online, hybrid, telecourse, on campus)
+			if (!String.IsNullOrWhiteSpace(f_online))
+			{
+				facets.Add(new ModalityFacet(ModalityFacet.Options.Online));
+			}
+			if (!String.IsNullOrWhiteSpace(f_hybrid))
+			{
+				facets.Add(new ModalityFacet(ModalityFacet.Options.Hybrid));
+			}
+			if (!String.IsNullOrWhiteSpace(f_telecourse))
+			{
+				facets.Add(new ModalityFacet(ModalityFacet.Options.Telecourse));
+			}
+			if (!String.IsNullOrWhiteSpace(f_oncampus))
+			{
+				facets.Add(new ModalityFacet(ModalityFacet.Options.OnCampus));
+			}
+
+			int startHour = 0;
+			int startMinute = 0;
+			int endHour = 23;
+			int endMinute = 59;
+
+			//determine integer values for start/end time hours and minutes
+			if (!String.IsNullOrWhiteSpace(timestart))
+			{
+				startHour = Convert.ToInt16(timestart.Substring(0, 2));
+				startMinute = Convert.ToInt16(timestart.Substring(3, 2));
+			}
+			if (!String.IsNullOrWhiteSpace(timeend))
+			{
+				endHour = Convert.ToInt16(timeend.Substring(0, 2));
+				endMinute = Convert.ToInt16(timeend.Substring(3, 2));
+			}
+
+			//add the time facet
+			facets.Add(new TimeFacet(new TimeSpan(startHour, startMinute, 0), new TimeSpan(endHour, endMinute, 0)));
+
+			//day of the week facets
+			if (!String.IsNullOrWhiteSpace(day_su))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Sunday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_m))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Monday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_t))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Tuesday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_w))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Wednesday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_th))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Thursday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_f))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Friday));
+			}
+			if (!String.IsNullOrWhiteSpace(day_s))
+			{
+				facets.Add(new DaysFacet(DaysFacet.Options.Saturday));
+			}
+
+			if (!String.IsNullOrWhiteSpace(avail))
+			{
+				if (avail == "All")
+				{
+					facets.Add(new AvailabilityFacet(AvailabilityFacet.Options.All));
+				}
+
+				if (avail == "Open")
+				{
+					facets.Add(new AvailabilityFacet(AvailabilityFacet.Options.Open));
+				}
+			}
+
+			return facets;
+		}
+
+		/// <summary>
+		/// Returns a friendly time in sentance form given a datetime. This value
+		/// is create by subtracting the input datetime from the current datetime.
+		/// example: 6/8/2011 07:23:123 -> about 4 hours ago
+		/// </summary>
+		public static string getFriendlyTime(DateTime theDate)
+		{
+
+			if (theDate == null)
+			{
+				return "last updated time unavailable";
+			}
+			else
+			{
 
 
+				const int SECOND = 1;
+				const int MINUTE = 60 * SECOND;
+				const int HOUR = 60 * MINUTE;
+				const int DAY = 24 * HOUR;
+				const int MONTH = 30 * DAY;
 
+				var deltaTimeSpan = new TimeSpan(DateTime.Now.Ticks - theDate.Ticks);
 
+				var delta = deltaTimeSpan.TotalSeconds;
 
+				if (delta < 0)
+				{
+					return "not yet";
+				}
+
+				if (delta < 1 * MINUTE)
+				{
+					return deltaTimeSpan.Seconds == 1 ? "one second ago" : deltaTimeSpan.Seconds + " seconds ago";
+				}
+				if (delta < 2 * MINUTE)
+				{
+					return "a minute ago";
+				}
+				if (delta < 45 * MINUTE)
+				{
+					return deltaTimeSpan.Minutes + " minutes ago";
+				}
+				if (delta < 90 * MINUTE)
+				{
+					return "an hour ago";
+				}
+				if (delta < 24 * HOUR)
+				{
+					return deltaTimeSpan.Hours + " hours ago";
+				}
+				if (delta < 48 * HOUR)
+				{
+					return "yesterday";
+				}
+				if (delta < 30 * DAY)
+				{
+					return deltaTimeSpan.Days + " days ago";
+				}
+				if (delta < 12 * MONTH)
+				{
+					int months = Convert.ToInt32(Math.Floor((double)deltaTimeSpan.Days / 30));
+					return months <= 1 ? "one month ago" : months + " months ago";
+				}
+				else
+				{
+					int years = Convert.ToInt32(Math.Floor((double)deltaTimeSpan.Days / 365));
+					return years <= 1 ? "one year ago" : years + " years ago";
+				}
+
+			}
+
+		}
+
+		/// <summary>
+		/// Returns true/false if the value passed is an integer
+		/// </summary>
+		public static bool IsInteger(string value)
+		{
+			try
+			{
+				Convert.ToInt32(value);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+
+		}
+
+		/// <summary>
+		/// Gets the current year given some input params. Helper method for getFriendlyDateFromYRQ
+		/// </summary>
+		public static string getYearHelper(string quarter, string year1, string year2, string decade, bool isLastTwoQuarters)
+		{
+			string first2OfYear = "";
+			string last2OfYear = "";
+			string ThirdOfYear = "";
+
+			int intYear1 = Convert.ToInt16(year1);
+			int intYear2 = Convert.ToInt16(year2);
+
+			switch (decade)
+			{
+				case "7":
+					first2OfYear = "19";
+					break;
+				case "8":
+					first2OfYear = "19";
+					break;
+				case "9":
+					first2OfYear = isLastTwoQuarters == true ? "20" : "19";
+					break;
+				case "A":
+					first2OfYear = "20";
+					break;
+				case "B":
+					first2OfYear = "20";
+					break;
+				case "C":
+					first2OfYear = "20";
+					break;
+				case "D":
+					first2OfYear = "20";
+					break;
+				default:
+					break;
+
+			}
+
+			switch (quarter)
+			{
+				case "1":
+					last2OfYear = getDecadeIntegerFromString(decade) + intYear1.ToString();
+					break;
+				case "2":
+					last2OfYear = getDecadeIntegerFromString(decade) + intYear1.ToString();
+					break;
+				case "3":
+					ThirdOfYear = isLastTwoQuarters == true ? getDecadeIntegerFromString(getNextDecade(decade)) : getDecadeIntegerFromString(decade);
+					last2OfYear = ThirdOfYear + intYear2.ToString();
+					break;
+				case "4":
+					ThirdOfYear = isLastTwoQuarters == true ? getDecadeIntegerFromString(getNextDecade(decade)) : getDecadeIntegerFromString(decade);
+					last2OfYear = ThirdOfYear + intYear2.ToString();
+					break;
+				default:
+
+					break;
+
+			}
+
+			return first2OfYear + last2OfYear;
+
+		}
+
+		/// <summary>
+		/// Gets the friendly decade value from the HP decade (A = 2000's, B = 2010's). Helper method for getYearHelper
+		/// </summary>
+		static public string getDecadeIntegerFromString(string decade)
+		{
+			switch (decade)
+			{
+				case "7":
+					return "7";
+				case "8":
+					return "8";
+				case "9":
+					return "9";
+				case "A":
+					return "0";
+				case "B":
+					return "1";
+				case "C":
+					return "2";
+				case "D":
+					return "3";
+			}
+			return "";
+		}
+
+		/// <summary>
+		/// Gets the next decade in HP format (8, 9, A, B)
+		/// </summary>
+		static public string getNextDecade(string decade)
+		{
+			switch (decade)
+			{
+				case "7":
+					return "8";
+				case "8":
+					return "9";
+				case "9":
+					return "A";
+				case "A":
+					return "B";
+				case "B":
+					return "C";
+				case "C":
+					return "D";
+				case "D":
+					return "E";
+			}
+			return "";
+
+		}
 	}
 }
--- CTCODS/CTCClassSchedule/Controllers/ClassesController.cs
+++ CTCODS/CTCClassSchedule/Controllers/ClassesController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using Ctc.Ods;
 using Ctc.Ods.Data;
 using Ctc.Ods.Types;
+using CTCClassSchedule.Common;
 using CTCClassSchedule.Models;
 using CTCClassSchedule.Properties;
 
@@ -103,7 +104,7 @@ namespace CTCClassSchedule.Controllers
 			ViewBag.avail = avail;
 
 			ViewBag.Title = "All " +  @Subject + " classes";
-			IList<ISectionFacet> facets = addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
+			IList<ISectionFacet> facets = Helpers.addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
 
 			ViewBag.ProgramUrl = getProgramUrl(Subject);
 
@@ -166,7 +167,7 @@ namespace CTCClassSchedule.Controllers
 			ViewBag.YRQ = yrq.ToString();
 			ViewBag.FriendlyYRQ = yrq.FriendlyName;
 			ViewBag.Title = ViewBag.Subject + "Classes for " + @ViewBag.Yearquarter;
-			IList<ISectionFacet> facets = addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
+			IList<ISectionFacet> facets = Helpers.addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
 
 			using (OdsRepository respository = new OdsRepository())
 			{
@@ -231,7 +232,7 @@ namespace CTCClassSchedule.Controllers
 			ViewBag.seatAvailbilityDisplayed = false;
 			ViewBag.Subject = Subject;
 			ViewBag.Title = @ViewBag.Yearquarter + " " + @Subject + " classes";
-			IList<ISectionFacet> facets = addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
+			IList<ISectionFacet> facets = Helpers.addFacets(timestart, timeend, day_su, day_m, day_t, day_w, day_th, day_f, day_s, f_oncampus, f_online, f_hybrid, f_telecourse, avail);
 
 			using (OdsRepository respository = new OdsRepository())
 			{
@@ -255,7 +256,7 @@ namespace CTCClassSchedule.Controllers
 											{
 													ParentObject = c,
 													SeatsAvailable = d.SeatsAvailable,
-													LastUpdated = getFriendlyTime(d.LastUpdated.GetValueOrDefault()),
+													LastUpdated = Helpers.getFriendlyTime(d.LastUpdated.GetValueOrDefault()),
 											}
 				               );
 
@@ -330,7 +331,7 @@ namespace CTCClassSchedule.Controllers
 									{
 																				ParentObject = c,
 											SeatsAvailable = d.SeatsAvailable,
-											LastUpdated = getFriendlyTime(d.LastUpdated.GetValueOrDefault()),
+											LastUpdated = Helpers.getFriendlyTime(d.LastUpdated.GetValueOrDefault()),
 									}
 					               );
 
@@ -398,7 +399,7 @@ namespace CTCClassSchedule.Controllers
 			foreach (var seat in seatsAvailable)
 			{
 				seats = seat.SeatsAvailable;
-				friendlyTime = getFriendlyTime(seat.LastUpdated.GetValueOrDefault());
+				friendlyTime = Helpers.getFriendlyTime(seat.LastUpdated.GetValueOrDefault());
 			}
 
 
@@ -444,7 +445,7 @@ namespace CTCClassSchedule.Controllers
 		/// Gets the course outcome information by scraping the Bellevue College
 		/// course outcomes website
 		/// </summary>
-		private dynamic getCourseOutcome(string Subject, string ClassNum)
+		private static dynamic getCourseOutcome(string Subject, string ClassNum)
 		{
 			string url = "http://bellevuecollege.edu/courseoutcomes/?CourseID=" + Subject + "%20" + ClassNum;
 			StringBuilder sb = new StringBuilder();
@@ -567,7 +568,7 @@ namespace CTCClassSchedule.Controllers
 			bool badlyFormedQuarter = false;
 			bool badlyFormedYear = false;
 
-			if (IsInteger(year))
+			if (Helpers.IsInteger(year))
 			{
 				if (Convert.ToInt16(year) < 1975 || Convert.ToInt16(year) > 2030)
 				{
@@ -687,314 +688,6 @@ namespace CTCClassSchedule.Controllers
 
 		}
 
-		/// <summary>
-		/// Gets the current year given some input params. Helper method for getFriendlyDateFromYRQ
-		/// </summary>
-		public string getYearHelper(string quarter, string year1, string year2, string decade, bool isLastTwoQuarters)
-		{
-			string first2OfYear = "";
-			string last2OfYear = "";
-			string ThirdOfYear = "";
-
-			int intYear1 = Convert.ToInt16(year1);
-			int intYear2 = Convert.ToInt16(year2);
-
-			switch (decade)
-			{
-				case "7":
-					first2OfYear = "19";
-					break;
-				case "8":
-					first2OfYear = "19";
-					break;
-				case "9":
-					first2OfYear = isLastTwoQuarters == true ? "20" : "19";
-					break;
-				case "A":
-					first2OfYear = "20";
-					break;
-				case "B":
-					first2OfYear = "20";
-					break;
-				case "C":
-					first2OfYear = "20";
-					break;
-				case "D":
-					first2OfYear = "20";
-					break;
-				default:
-					break;
-
-			}
-
-			switch (quarter)
-			{
-				case "1":
-					last2OfYear = getDecadeIntegerFromString(decade) + intYear1.ToString();
-					break;
-				case "2":
-					last2OfYear = getDecadeIntegerFromString(decade) + intYear1.ToString();
-					break;
-				case "3":
-					ThirdOfYear = isLastTwoQuarters == true ? getDecadeIntegerFromString(getNextDecade(decade)) : getDecadeIntegerFromString(decade);
-					last2OfYear = ThirdOfYear + intYear2.ToString();
-					break;
-				case "4":
-					ThirdOfYear = isLastTwoQuarters == true ? getDecadeIntegerFromString(getNextDecade(decade)) : getDecadeIntegerFromString(decade);
-					last2OfYear = ThirdOfYear + intYear2.ToString();
-					break;
-				default:
-
-					break;
-
-			}
-
-			return first2OfYear + last2OfYear;
-
-		}
-
-		/// <summary>
-		/// Gets the friendly decade value from the HP decade (A = 2000's, B = 2010's). Helper method for getYearHelper
-		/// </summary>
-		private string getDecadeIntegerFromString(string decade)
-		{
-			switch (decade)
-			{
-				case "7":
-					return "7";
-				case "8":
-					return "8";
-				case "9":
-					return "9";
-				case "A":
-					return "0";
-				case "B":
-					return "1";
-				case "C":
-					return "2";
-				case "D":
-					return "3";
-			}
-			return "";
-		}
-
-		/// <summary>
-		/// Gets the next decade in HP format (8, 9, A, B)
-		/// </summary>
-		private string getNextDecade(string decade)
-		{
-			switch (decade)
-			{
-				case "7":
-					return "8";
-				case "8":
-					return "9";
-				case "9":
-					return "A";
-				case "A":
-					return "B";
-				case "B":
-					return "C";
-				case "C":
-					return "D";
-				case "D":
-					return "E";
-			}
-			return "";
-
-		}
-
-		/// <summary>
-		/// Returns true/false if the value passed is an integer
-		/// </summary>
-		public static bool IsInteger(string value)
-		{
-			try
-			{
-				Convert.ToInt32(value);
-				return true;
-			}
-			catch
-			{
-				return false;
-			}
-
-		}
-
-		/// <summary>
-		/// Returns a friendly time in sentance form given a datetime. This value
-		/// is create by subtracting the input datetime from the current datetime.
-		/// example: 6/8/2011 07:23:123 -> about 4 hours ago
-		/// </summary>
-		public string getFriendlyTime(DateTime theDate)
-		{
-
-			if (theDate == null)
-			{
-				return "last updated time unavailable";
-			}
-			else
-			{
-
-
-				const int SECOND = 1;
-				const int MINUTE = 60 * SECOND;
-				const int HOUR = 60 * MINUTE;
-				const int DAY = 24 * HOUR;
-				const int MONTH = 30 * DAY;
-
-				var deltaTimeSpan = new TimeSpan(DateTime.Now.Ticks - theDate.Ticks);
-
-				var delta = deltaTimeSpan.TotalSeconds;
-
-				if (delta < 0)
-				{
-					return "not yet";
-				}
-
-				if (delta < 1 * MINUTE)
-				{
-					return deltaTimeSpan.Seconds == 1 ? "one second ago" : deltaTimeSpan.Seconds + " seconds ago";
-				}
-				if (delta < 2 * MINUTE)
-				{
-					return "a minute ago";
-				}
-				if (delta < 45 * MINUTE)
-				{
-					return deltaTimeSpan.Minutes + " minutes ago";
-				}
-				if (delta < 90 * MINUTE)
-				{
-					return "an hour ago";
-				}
-				if (delta < 24 * HOUR)
-				{
-					return deltaTimeSpan.Hours + " hours ago";
-				}
-				if (delta < 48 * HOUR)
-				{
-					return "yesterday";
-				}
-				if (delta < 30 * DAY)
-				{
-					return deltaTimeSpan.Days + " days ago";
-				}
-				if (delta < 12 * MONTH)
-				{
-					int months = Convert.ToInt32(Math.Floor((double)deltaTimeSpan.Days / 30));
-					return months <= 1 ? "one month ago" : months + " months ago";
-				}
-				else
-				{
-					int years = Convert.ToInt32(Math.Floor((double)deltaTimeSpan.Days / 365));
-					return years <= 1 ? "one year ago" : years + " years ago";
-				}
-
-			}
-
-		}
-
-		/// <summary>
-		/// returns an IList<ISectionFacet> that contains all of the facet information
-		/// passed into the app by the user clicking on the faceted search left pane
-		/// facets accepted: flex, time, days, availability
-		/// </summary>
-		private IList<ISectionFacet> addFacets(string timestart, string timeend, string day_su, string day_m, string day_t, string day_w, string day_th, string day_f, string day_s, string f_oncampus, string f_online, string f_hybrid, string f_telecourse, string avail)
-		{
-			IList<ISectionFacet> facets = new List<ISectionFacet>();
-
-			//add the class format facet options (online, hybrid, telecourse, on campus)
-			if (!string.IsNullOrWhiteSpace(f_online))
-			{
-				facets.Add(new ModalityFacet(ModalityFacet.Options.Online));
-			}
-			if (!string.IsNullOrWhiteSpace(f_hybrid))
-			{
-				facets.Add(new ModalityFacet(ModalityFacet.Options.Hybrid));
-			}
-			if (!string.IsNullOrWhiteSpace(f_telecourse))
-			{
-				facets.Add(new ModalityFacet(ModalityFacet.Options.Telecourse));
-			}
-			if (!string.IsNullOrWhiteSpace(f_oncampus))
-			{
-				facets.Add(new ModalityFacet(ModalityFacet.Options.OnCampus));
-			}
-
-			int startHour = 0;
-			int startMinute = 0;
-			int endHour = 23;
-			int endMinute = 59;
-
-			//determine integer values for start/end time hours and minutes
-			if (!string.IsNullOrWhiteSpace(timestart))
-			{
-				startHour = Convert.ToInt16(timestart.Substring(0, 2));
-				startMinute = Convert.ToInt16(timestart.Substring(3, 2));
-			}
-			if (!string.IsNullOrWhiteSpace(timeend))
-			{
-				endHour = Convert.ToInt16(timeend.Substring(0, 2));
-				endMinute = Convert.ToInt16(timeend.Substring(3, 2));
-			}
-
-			//add the time facet
-			facets.Add(new TimeFacet(new TimeSpan(startHour, startMinute, 0), new TimeSpan(endHour, endMinute, 0)));
-
-
-
-			//day of the week facets
-			if (!string.IsNullOrWhiteSpace(day_su))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Sunday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_m))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Monday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_t))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Tuesday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_w))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Wednesday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_th))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Thursday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_f))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Friday));
-			}
-			if (!string.IsNullOrWhiteSpace(day_s))
-			{
-				facets.Add(new DaysFacet(DaysFacet.Options.Saturday));
-			}
-
-
-			if (!string.IsNullOrWhiteSpace(avail))
-			{
-				if (avail == "All")
-				{
-					facets.Add(new AvailabilityFacet(AvailabilityFacet.Options.All));
-				}
-
-				if (avail == "Open")
-				{
-					facets.Add(new AvailabilityFacet(AvailabilityFacet.Options.Open));
-				}
-			}
-
-
-
-
-
-			return facets;
-		}
-
 		private string getProgramUrl(string Subject)
 		{
 			string ProgramURL = "";
@@ -1002,6 +695,7 @@ namespace CTCClassSchedule.Controllers
 																where s.Abbreviation == Subject
 																select s;
 
+			// TODO: should this be a loop? Only the last item is being used.
 			foreach (ProgramInformation program in specificProgramInfo)
 			{
 				ProgramURL = program.Url;
@@ -1010,6 +704,7 @@ namespace CTCClassSchedule.Controllers
 
 			//if the url is a fully qualified url (e.g. http://continuinged.bellevuecollege.edu/about)
 			//just return it, otherwise prepend iwth the current school url.
+			// TODO: should this be .StartsWith()?
 			if (ProgramURL.Contains("http://"))
 			{
 				return ProgramURL;
