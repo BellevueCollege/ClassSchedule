a0d0758d2df46d40f9303375e06f121d25973e88
Shawn South <shawn.south@bellevuecollege.edu>
2011-09-13 19:07:15 -0700

    some tweaking to improve the performance of the
    code.

--- CTCODS/CTCClassSchedule/CourseHPQuery.cs
+++ CTCODS/CTCClassSchedule/CourseHPQuery.cs
@@ -9,66 +9,56 @@ namespace CTCClassSchedule
 {
 	public class CourseHPQuery
 	{
-    public WebRequest request;
-    public string result, seatsAvailable;
-    public int index, seats;
-
-    //really exciting constructor
-    public CourseHPQuery()
-    {
-
-    }
-
-    //code to grab the open seats and return as string
+		//code to grab the open seats and return as string
     public int findOpenSeats (string itemNumber, string YRQ)
     {
 			//much of this is from: http://msdn.microsoft.com/en-us/library/debx8sh9.aspx
 
-      string postData;
-			string colCode = ConfigurationManager.AppSettings["collegeCode"]; // College Code to construct URL
-
+			// TODO: gracefully handle missing AppSetting value
+	string colCode = ConfigurationManager.AppSettings["collegeCode"]; // College Code to construct URL
 
       //set the location the post is going to
-      request = WebRequest.Create("https://www.ctc.edu/cgi-bin/rq" + colCode);
+	WebRequest request = WebRequest.Create("https://www.ctc.edu/cgi-bin/rq" + colCode);
 			request.Method = "POST";
-      postData = getPostData(itemNumber, YRQ);
+			request.ContentType = "application/x-www-form-urlencoded";
 
+			string postData = getPostData(itemNumber, YRQ);
       byte[] byteArray = Encoding.UTF8.GetBytes(postData);
 
-			// Set the ContentType property of the WebRequest.
-			request.ContentType = "application/x-www-form-urlencoded";
-			// Set the ContentLength property of the WebRequest.
 			request.ContentLength = byteArray.Length;
-			// Get the request stream.
-			Stream dataStream = request.GetRequestStream();
-			// Write the data to the request stream.
-			dataStream.Write(byteArray, 0, byteArray.Length);
-			// Close the Stream object.
-			dataStream.Close();
-			// Get the response.
-			WebResponse response = request.GetResponse();
-			// Display the status.
+
+	string result;
+
+	using (Stream requestStream = request.GetRequestStream())
+	{
+		requestStream.Write(byteArray, 0, byteArray.Length);
+				requestStream.Close();	// do we need to close the stream before we can GetResponse()?
+
+		// Get the response.
+		using (WebResponse response = request.GetResponse())
+		{
+			// TODO: why are we writing out to the console here?
 			Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-			// Get the stream containing content returned by the server.
-			dataStream = response.GetResponseStream();
-			// Open the stream using a StreamReader for easy access.
-			StreamReader reader = new StreamReader(dataStream);
-			// Read the content.
-			result = reader.ReadToEnd();
 
-			// Clean up the streams.
-			reader.Close();
-			dataStream.Close();
-			response.Close();
+					// TODO: verify response is not null
+			using (Stream responseStream = response.GetResponseStream())
+			{
+				using (StreamReader reader = new StreamReader(responseStream))
+				{
+					result = reader.ReadToEnd();
+				}
+			}
+		}
+	}
 
-      //grab the start of the 'seats available' string
-      index = result.IndexOf("Seats Available: ");
+	//grab the start of the 'seats available' string
+      int index = result.IndexOf("Seats Available: ");
 
       //if 'seats available' was found
       if (index != -1)
       {
-        seatsAvailable = result.Substring(index + 17, 3);
-        seats = Convert.ToInt16(seatsAvailable);
+        string seatsAvailable = result.Substring(index + 17, 3);
+        int seats = Convert.ToInt16(seatsAvailable);
 
         return seats;
       }
@@ -127,7 +117,7 @@ namespace CTCClassSchedule
       yearData += (yearDataInt + Int32.Parse(yearFrom) + 1).ToString()[2];
       yearData += (yearDataInt + Int32.Parse(yearFrom) + 1).ToString()[3];
 
-
+			// TODO: gracefully handle missing AppSetting value
 			string schoolURL = ConfigurationManager.AppSettings["currentSchoolUrl"]; // School URL for returnURL
 			string scheduleDir = ConfigurationManager.AppSettings["currentAppSubdirectory"]; // App SubDir for returnURL
 
