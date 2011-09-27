25b55959138b70bfa438356ab220044ea9ef9ad2
Shawn South <shawn.south@bellevuecollege.edu>
2011-09-26 17:31:45 -0700

    Fleshed out CreditsValue extension method to
    support non-nullable decimal values and make html message (when value is
    null) optional.

--- CTCODS/CTCClassSchedule/Common/HelperExtensions.cs
+++ CTCODS/CTCClassSchedule/Common/HelperExtensions.cs
@@ -1,14 +1,56 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using Ctc.Ods.Types;
 
 namespace CTCClassSchedule.Common
 {
+	/// <summary>
+	/// Contains extension methods for the <see cref="HtmlHelper"/> class
+	/// </summary>
 	public static class Html
 	{
-		public static IHtmlString CreditsValue(this HtmlHelper html, decimal? value, string errorHtml)
+		#region CreditsValue()
+		/// <summary>
+		/// Formats a <see cref="Course"/> credits for display to the user
+		/// </summary>
+		/// <param name="html"></param>
+		/// <param name="credits">The credits value to format</param>
+		/// <param name="nullMessage">Optional HTML to display if <paramref name="credits"/> is <i>null</i></param>
+		/// <returns>An integer value if only zeroes follow the decimal point. Otherwise, the original decimal value.</returns>
+		/// <remarks>
+		/// This method removes decimal information if the value is a whole number.
+		/// </remarks>
+		public static IHtmlString CreditsValue(this HtmlHelper html, decimal? credits, string nullMessage = null)
 		{
-			string output = value.HasValue ? value.Value.ToString().TrimEnd('0').TrimEnd('.') : errorHtml;
+			string output = credits.HasValue ? TrimCreditsValue(credits.Value) : nullMessage ?? string.Empty;
 			return html.Raw(output);
 		}
+
+		/// <summary>
+		/// Formats a <see cref="Course"/> credits for display to the user
+		/// </summary>
+		/// <param name="html"></param>
+		/// <param name="credits">The credits value to format</param>
+		/// <returns></returns>
+		/// <remarks>
+		/// This method removes decimal information if the value is a whole number.
+		/// </remarks>
+		public static IHtmlString CreditsValue(this HtmlHelper html, decimal credits)
+		{
+			string output = TrimCreditsValue(credits);
+			return html.Raw(output);
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <param name="credits"></param>
+		/// <returns></returns>
+		private static string TrimCreditsValue(decimal credits)
+		{
+			return credits.ToString().TrimEnd('0').TrimEnd('.');
+		}
+
+		#endregion
 	}
 }
