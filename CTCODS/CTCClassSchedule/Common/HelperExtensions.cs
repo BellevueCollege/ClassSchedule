1ee09251ff53c8bcab6033251a792dedfd0dc950
Shawn South <shawn.south@bellevuecollege.edu>
2011-09-26 17:02:10 -0700

    Added course title, credits and link to "classes
    not currently offered" section of search results.

---
+++ CTCODS/CTCClassSchedule/Common/HelperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CTCClassSchedule.Common
+{
+	public static class Html
+	{
+		public static IHtmlString CreditsValue(this HtmlHelper html, decimal? value, string errorHtml)
+		{
+			string output = value.HasValue ? value.Value.ToString().TrimEnd('0').TrimEnd('.') : errorHtml;
+			return html.Raw(output);
+		}
+	}
+}
--- CTCODS/CTCClassSchedule/Common/SearchResultsNoSection.cs
+++ CTCODS/CTCClassSchedule/Common/SearchResultsNoSection.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace CTCClassSchedule.Common
 {
 	public class SearchResultNoSection
 	{
 
 		public string CourseID { get; set; }
+		public string CourseTitle {get;set;}
+		public decimal? Credits {get;set;}
 		public string CourseDescription { get; set; }
 		public int? SearchRank { get; set; }
 	}
