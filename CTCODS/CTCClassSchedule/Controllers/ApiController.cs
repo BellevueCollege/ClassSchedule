caa6101a0dcbf1c7823e07811614adc768787cc3
Shawn South <shawn.south@bellevuecollege.edu>
2011-10-19 18:28:11 -0700

    Updated subject listings to use data from
    ProgramInformation table for the app.

--- CTCODS/CTCClassSchedule/Controllers/ApiController.cs
+++ CTCODS/CTCClassSchedule/Controllers/ApiController.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Ctc.Ods.Data;
 using Ctc.Ods.Types;
+using CTCClassSchedule.Common;
+using CTCClassSchedule.Models;
 
 namespace CTCClassSchedule.Controllers
 {
 	public class ApiController : Controller
 	{
+		private ClassScheduleDevProgramEntities _programdb = new ClassScheduleDevProgramEntities();
+
 		//
 		// GET: /Api/Subjects
 		/// <summary>
 		/// Returns an array of <see cref="Course"/> Subjects
 		/// </summary>
 		/// <param name="format"></param>
-		///<param name="yrq"></param>
+		///<param name="YearQuarter"></param>
 		///<returns>
 		///		Either a <see cref="PartialViewResult"/> which can be embedded in an MVC View,
 		///		or the list of <see cref="Course"/> Subjects as a JSON array.
@@ -25,41 +30,38 @@ namespace CTCClassSchedule.Controllers
 		///		</example>
 		/// </remarks>
 		[HttpGet]
-		public ActionResult Subjects(string format, string yrq)
+		public ActionResult Subjects(string format, string YearQuarter)
 		{
 			using (OdsRepository db = new OdsRepository(HttpContext))
 			{
 				IList<CoursePrefix> data;
-				data = string.IsNullOrWhiteSpace(yrq) || yrq.ToUpper() == "ALL" ? db.GetCourseSubjects() : db.GetCourseSubjects(YearQuarter.FromFriendlyName(yrq));
+				data = string.IsNullOrWhiteSpace(YearQuarter) || YearQuarter.ToUpper() == "ALL" ? db.GetCourseSubjects() : db.GetCourseSubjects(Ctc.Ods.Types.YearQuarter.FromFriendlyName(YearQuarter));
+
+				IList<ProgramInformation> progInfo = (from s in _programdb.ProgramInformation
+																							select s).ToList();
+
+				IList<ScheduleCoursePrefix> subjectList = (from p in progInfo
+																									where data.Select(c => c.Subject).Contains(p.Abbreviation.TrimEnd('&'))
+																									select new ScheduleCoursePrefix
+																														{
+																															Subject = p.URL,
+																															Title = p.Title
+																														})
+																									.OrderBy(s => s.Title)
+																									.Distinct()
+																									.ToList();
 
 				if (format == "json")
 				{
-					// NOTE: AllowGet exposes a potential for JSON Hijacking (see http://haacked.com/archive/2009/06/25/json-hijacking.aspx)
+					// NOTE: AllowGet exposes the potential for JSON Hijacking (see http://haacked.com/archive/2009/06/25/json-hijacking.aspx)
 					// but is not an issue here because we are receiving and returning public (e.g. non-sensitive) data
-					return Json(data, JsonRequestBehavior.AllowGet);
+					return Json(subjectList, JsonRequestBehavior.AllowGet);
 				}
 
-				IDictionary<string, object> linkParams = new Dictionary<string, object>(Request.QueryString.Count);
-				foreach (string key in Request.QueryString.AllKeys)
-				{
-					if (key != "X-Requested-With")
-					{
-						string value = Request.QueryString[key];
-
-						if (key == "yrq" && value.ToUpper() != "ALL")
-						{
-							ViewBag.YearQuarter =  YearQuarter.FromFriendlyName(value);
-						}
-						else
-						{
-							linkParams.Add(key, value);
-						}
-					}
-				}
-				ViewBag.LinkParams = linkParams;
+				ViewBag.LinkParams = Helpers.getLinkParams(Request);
 				ViewBag.SubjectsColumns = 2;
 
-				return PartialView(data);
+				return PartialView(subjectList);
 			}
 		}
 	}
