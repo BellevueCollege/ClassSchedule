9efd3cd0b51b0e92e5e79af9c659a59633745e72
Shawn South <shawn.south@bellevuecollege.edu>
2011-11-03 12:08:09 -0700

    Fixed error pages not being displayed propertly.

--- CTCODS/CTCClassSchedule/Global.asax.cs
+++ CTCODS/CTCClassSchedule/Global.asax.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
-using MvcMiniProfiler;
 
 namespace CTCClassSchedule
 {
@@ -13,10 +11,7 @@ namespace CTCClassSchedule
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			filters.Add(new HandleErrorAttribute
-					{
-							View = "ScheduleError"
-					});
+			filters.Add(new HandleErrorAttribute());
 		}
 
 		public static void RegisterRoutes(RouteCollection routes)
