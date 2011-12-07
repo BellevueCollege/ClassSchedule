f804395f5928cecde5eb34375f1fe45b9d0cf4fa
Shawn South <shawn.south@bellevuecollege.edu>
2011-12-07 15:16:57 -0800

    Added check to insure missing SectionFootnotes
    always come through as an empty string (rather than a null).

--- CTCODS/CTCClassSchedule/Common/Helpers.cs
+++ CTCODS/CTCClassSchedule/Common/Helpers.cs
@@ -464,8 +464,8 @@ namespace CTCClassSchedule.Common
 						    ParentObject = c,
 						    SeatsAvailable = d != null ? d.SeatsAvailable : int.MinValue,	// allows us to identify past quarters (with no availability info)
 						    LastUpdated = getFriendlyTime(d != null ? d.LastUpdated.GetValueOrDefault() : DateTime.MinValue),
-						    SectionFootnotes = d != null ? d.SectionFootnote : string.Empty,
-						    CourseFootnotes = d != null ? d.CourseFootnote : string.Empty
+						    SectionFootnotes = d != null ? d.SectionFootnote ?? string.Empty : string.Empty,
+						    CourseFootnotes = d != null ? d.CourseFootnote ?? string.Empty : string.Empty
 						}).ToList();
       }
 
