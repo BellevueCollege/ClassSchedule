a7a4f841a648c0be68bff5543072cefde7225cf5
Nathan Gudmunson <nathan.gudmunson@bellevuecollege.edu>
2012-01-10 08:29:35 -0800

    (empty)

--- CTCODS/CTCClassSchedule/Controllers/ApiController.cs
+++ CTCODS/CTCClassSchedule/Controllers/ApiController.cs
@@ -70,5 +70,43 @@ namespace CTCClassSchedule.Controllers
 				return PartialView(subjectList);
 			}
 		}
+
+		public ActionResult SectionEdit(string itemNumber, string format = "json")
+		{
+			using (OdsRepository db = new OdsRepository(HttpContext))
+			{
+				IList<CoursePrefix> data;
+				data = db.GetCourseSubjects();
+
+				IList<vw_ProgramInformation> progInfo;
+				using (ClassScheduleDb classScheduleDb = new ClassScheduleDb())
+				{
+					progInfo = (from s in classScheduleDb.vw_ProgramInformation select s).ToList();
+				}
+				IList<ScheduleCoursePrefix> subjectList = (from p in progInfo
+																									 where data.Select(c => c.Subject).Contains(p.Abbreviation.TrimEnd('&'))
+																									 select new ScheduleCoursePrefix
+																									 {
+																										 Subject = p.URL,
+																										 Title = p.Title
+																									 })
+																									.OrderBy(s => s.Title)
+																									.Distinct()
+																									.ToList();
+
+				if (format == "json")
+				{
+					// NOTE: AllowGet exposes the potential for JSON Hijacking (see http://haacked.com/archive/2009/06/25/json-hijacking.aspx)
+					// but is not an issue here because we are receiving and returning public (e.g. non-sensitive) data
+					return Json(subjectList, JsonRequestBehavior.AllowGet);
+				}
+
+				ViewBag.LinkParams = Helpers.getLinkParams(Request);
+				ViewBag.SubjectsColumns = 2;
+
+				return PartialView(subjectList);
+			}
+		}
+
 	}
 }
