ed43f6f5dc73baebd974d3200c57b83e63f5eaf3
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2012-01-27 13:11:48 -0800

    (empty)

--- CTCODS/CTCClassSchedule/Common/AutomatedFootnotesConfig.cs
+++ CTCODS/CTCClassSchedule/Common/AutomatedFootnotesConfig.cs
@@ -16,10 +16,41 @@ namespace CTCClassSchedule
             _footnoteInstances = GetFootnoteInstances(sec.FootnoteInstances);
         }
 
+
+
         public static AutomatedFootnoteElement Footnotes(string footnoteName)
         {
             return _footnoteInstances[footnoteName];
         }
+
+        public static string getAutomatedFootnotesText(SectionWithSeats section)
+        {
+            string footnoteTextResult = string.Empty;
+            string dateParam = "{DATE}";
+            string dateText;
+            AutomatedFootnoteElement footnote;
+            string wSpace = section.Footnotes.Count() == 0 ? string.Empty : " ";
+            if (section.IsLateStart)
+            {
+                footnote = Footnotes("lateStart");
+                dateText = section.StartDate.GetValueOrDefault(DateTime.Now).ToString(footnote.StringFormat);
+                footnoteTextResult += wSpace + Footnotes("lateStart").Text.Replace(dateParam, dateText);
+            }
+            // add diff end date flag
+            if (section.IsHybrid)
+            {
+                footnoteTextResult += wSpace + Footnotes("hybrid").Text;
+            }
+            if (section.IsContinuousEnrollment)
+            {
+                footnote = Footnotes("continuousEnrollment");
+                dateText = section.EndDate.GetValueOrDefault(DateTime.Now).ToString(footnote.StringFormat);
+                footnoteTextResult += wSpace + Footnotes("continuousEnrollment").Text.Replace(dateParam, dateText);
+            }
+
+            return footnoteTextResult;
+        }
+
 
         private static Dictionary<string, AutomatedFootnoteElement> GetFootnoteInstances(AutomatedFootnoteCollection collection)
         {
