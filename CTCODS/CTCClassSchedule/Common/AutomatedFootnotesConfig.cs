31dbe10f6fc4b2e439361d1eb4b43b30ee451651
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2012-01-27 12:33:20 -0800

    Added custom section in the Web.config to define
    the text that each automated footnote will have.

---
+++ CTCODS/CTCClassSchedule/Common/AutomatedFootnotesConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CTCClassSchedule
+{
+    public class AutomatedFootnotesConfig
+    {
+        protected static Dictionary<string, AutomatedFootnoteElement> _footnoteInstances;
+
+        static AutomatedFootnotesConfig()
+        {
+            AutomatedFootnotesSection sec = (AutomatedFootnotesSection)System.Configuration.ConfigurationManager.GetSection("ctcAutomatedFootnoteSettings");
+
+            _footnoteInstances = GetFootnoteInstances(sec.FootnoteInstances);
+        }
+
+        public static AutomatedFootnoteElement Footnotes(string footnoteName)
+        {
+            return _footnoteInstances[footnoteName];
+        }
+
+        private static Dictionary<string, AutomatedFootnoteElement> GetFootnoteInstances(AutomatedFootnoteCollection collection)
+        {
+            Dictionary<string, AutomatedFootnoteElement> instances = new Dictionary<string, AutomatedFootnoteElement>();
+
+            foreach (AutomatedFootnoteElement i in collection)
+            {
+                instances.Add(i.Name, i);
+            }
+
+            return instances;
+        }
+    }
+}
---
+++ CTCODS/CTCClassSchedule/Common/AutomatedFootnotesSection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace CTCClassSchedule
+{
+    public class AutomatedFootnotesSection : ConfigurationSection
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        [ConfigurationProperty("", IsRequired = false, IsDefaultCollection = true)]
+        public AutomatedFootnoteCollection FootnoteInstances
+        {
+            get { return (AutomatedFootnoteCollection)this[""]; }
+            set { this[""] = value; }
+        }
+    }
+
+    public class AutomatedFootnoteCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AutomatedFootnoteElement();
+        }
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AutomatedFootnoteElement)element).Name;
+        }
+    }
+
+    public class AutomatedFootnoteElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey = true, IsRequired = true)]
+        public string Name
+        {
+            get { return (string)base["name"]; }
+            set { base["name"] = value; }
+        }
+
+        [ConfigurationProperty("text", IsRequired = true)]
+        public string Text
+        {
+            get { return (string)base["text"]; }
+            set { base["text"] = value; }
+        }
+
+        [ConfigurationProperty("stringFormat", IsRequired = false)]
+        public string StringFormat
+        {
+            get { return (string)base["stringFormat"]; }
+            set { base["stringFormat"] = value; }
+        }
+    }
+}
