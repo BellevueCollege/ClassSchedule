7e6592500b19732b2b3ca7c600003c29e6e40af6
Shawn South <shawn.south@bellevuecollege.edu>
2012-02-16 18:43:50 -0800

    Incremented version for release to test.

--- CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
+++ CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CTCClassSchedule")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Online class schedule dynamically generated from system data.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bellevue College")]
 [assembly: AssemblyProduct("CTCClassSchedule")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.2")]
-[assembly: AssemblyFileVersion("1.0.2.2")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
