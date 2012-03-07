5acb2fadfd27db331c2ebdae7bbc1320e2627371
Shawn South <shawn.south@bellevuecollege.edu>
2012-03-07 15:27:35 -0800

    re-worked code so it is easier to read.

--- CTCODS/CTCClassSchedule.DB/Views/dbo.vw_ClassScheduleData.sql
+++ CTCODS/CTCClassSchedule.DB/Views/dbo.vw_ClassScheduleData.sql
@@ -1,27 +1,56 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-/*where Right(c.ClassID, 4)='B122'*/
 CREATE VIEW [dbo].[vw_ClassScheduleData]
 AS
-SELECT     c.ClassID, c.YearQuarterID, c.CourseID, CASE WHEN isNULL(u.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(sa.LastUpdated, DATEADD(day, - 2, getdate()))
-                      THEN CASE WHEN c.ClassCapacity - c.StudentsEnrolled < 0 THEN 0 ELSE c.ClassCapacity - c.StudentsEnrolled END ELSE sa.SeatsAvailable END AS SeatsAvailable,
-                      CASE WHEN isNULL(u.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(sa.LastUpdated, DATEADD(day, - 2, getdate()))
-                      THEN u.LastUpdated ELSE sa.LastUpdated END AS LastUpdated, cf.Footnote AS CourseFootnote, sf.Footnote AS SectionFootnote, p.Abbreviation, p.ProgramURL,
-                      p.Title, p.URL, p.Division, p.ContactName, p.ContactPhone, p.Intro, sf.CustomTitle, sf.CustomDescription
-FROM         dbo.vw_Class AS c LEFT OUTER JOIN
-                      dbo.vw_SeatAvailability AS sa ON c.ClassID = sa.ClassID LEFT OUTER JOIN
-                      dbo.SectionFootnote AS sf ON c.ClassID = sf.ClassID LEFT OUTER JOIN
-                      dbo.CourseFootnote AS cf ON c.CourseID = cf.CourseID LEFT OUTER JOIN
-                      dbo.ProgramInformation AS p ON c.Department = p.Abbreviation LEFT OUTER JOIN
-                          (SELECT     TOP (1) UpdateDate AS LastUpdated
-                            FROM          ODS.dbo.vw_TableTransferLog
-                            WHERE      (ODSTableName = 'Class')
-                            ORDER BY LastUpdated DESC) AS u ON 1 = 1
+SELECT
+	c.ClassID
+	,c.YearQuarterID
+	,c.CourseID
+	,CASE
+		WHEN isNULL(u.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(sa.LastUpdated, DATEADD(day, - 2, getdate()))
+			THEN CASE
+				WHEN c.ClassCapacity - c.StudentsEnrolled < 0
+					THEN 0
+				ELSE c.ClassCapacity - c.StudentsEnrolled
+			END
+		ELSE sa.SeatsAvailable
+	END AS SeatsAvailable
+	,CASE
+		WHEN isNULL(u.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(sa.LastUpdated, DATEADD(day, - 2, getdate()))
+			THEN u.LastUpdated
+		ELSE sa.LastUpdated
+	END AS LastUpdated
+	,cf.Footnote AS CourseFootnote
+	,sf.Footnote AS SectionFootnote
+	,p.Abbreviation
+	,p.ProgramURL
+	,p.Title
+	,p.URL
+	,p.Division
+	,p.ContactName
+	,p.ContactPhone
+	,p.Intro
+	,sf.CustomTitle
+	,sf.CustomDescription
+FROM dbo.vw_Class AS c
+LEFT OUTER JOIN dbo.vw_SeatAvailability AS sa ON c.ClassID = sa.ClassID
+LEFT OUTER JOIN dbo.SectionFootnote AS sf ON c.ClassID = sf.ClassID
+LEFT OUTER JOIN dbo.CourseFootnote AS cf ON c.CourseID = cf.CourseID
+LEFT OUTER JOIN dbo.ProgramInformation AS p ON c.Department = p.Abbreviation
+LEFT OUTER JOIN (
+	SELECT TOP (1)
+		UpdateDate AS LastUpdated
+	FROM ODS.dbo.vw_TableTransferLog
+	WHERE (ODSTableName = 'Class')
+	ORDER BY LastUpdated DESC
+) AS u ON 1 = 1
 
 GO
+
 GRANT SELECT ON  [dbo].[vw_ClassScheduleData] TO [WebApplicationUser]
 GO
 EXEC sp_addextendedproperty N'MS_DiagramPane1', N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
