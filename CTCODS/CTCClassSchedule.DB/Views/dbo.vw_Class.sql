41264cdd9ce47a3f147f4ea121911a2251aa9713
Shawn South <shawn.south@bellevuecollege.edu>
2012-03-07 15:24:03 -0800

    Added calculation of class capacity and
    enrollments, including support for clustered classes.

--- CTCODS/CTCClassSchedule.DB/Views/dbo.vw_Class.sql
+++ CTCODS/CTCClassSchedule.DB/Views/dbo.vw_Class.sql
@@ -1,41 +1,44 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-
-
-
 CREATE view [dbo].[vw_Class]
 AS
-SELECT [ClassID]
-      ,[YearQuarterID]
-      ,[ItemNumber]
-      ,[CourseID]
-      ,[Department]
-      ,[CourseNumber]
-      ,[ClassCapacity]
-      ,[StudentsEnrolled]
-      /*
-	,CASE
-		WHEN ClassCapacity - StudentsEnrolled < 0 then 0
-		ELSE ClassCapacity - StudentsEnrolled
-	END as SeatsAvailable
-	*/
-	  --,Cast('11/1/2011 11:00:000 AM' as datetime) as LastUpdated --u.[UpdateDate] as LastUpdated
+SELECT c.[ClassID]
+      ,c.[YearQuarterID]
+      ,c.[ItemNumber]
+      ,c.[CourseID]
+      ,c.[Department]
+      ,c.[CourseNumber]
+-- NOTE: The capacity/enrollment logic only works for B013 (Spring 2011) and later.
+-- Previous to that, associated data in the cluster table is NULL. - shawn.south@bellevuecollege.edu
+      ,CASE
+		WHEN NOT c.[ClusterItemNumber] IS NULL
+			THEN l.ClusterCapacity
+			ELSE c.[ClassCapacity]
+		END AS ClassCapacity
+      ,CASE
+		WHEN NOT c.[ClusterItemNumber] IS NULL
+			THEN l.ClusterEnrolled
+			ELSE c.[StudentsEnrolled]
+		END AS StudentsEnrolled
+/* COMMENT THIS LINE FOR DEBUGGING
+	  ,c.ClusterItemNumber AS "(ClusterItemNumber)"
+	  ,c.ClassCapacity AS "(ClassCapacity)"
+	  ,c.StudentsEnrolled AS "(StudentsEnrolled)"
+	  ,l.ClusterCapacity AS "(ClusterCapacity)"
+	  ,l.ClusterEnrolled AS "(ClusterEnrolled)"
+--*/
   FROM [ODS].[dbo].[vw_Class] c
-  /*
-	left outer join
-		(
-		select top 1 UpdateDate
-		from ODS.dbo.vw_TableTransferLog
-		where ODSTableName='Class'
-		order by UpdateDate DESC) u on 1=1
-		*/
-
-
-
-
+  LEFT JOIN [ODS].[dbo].[vw_ClassCluster] l ON l.ClusterItemNumber = c.ClusterItemNumber AND l.YearQuarterID = c.YearQuarterID
+/* COMMENT THIS LINE FOR DEBUGGING
+  WHERE NOT c.ClusterItemNumber IS NULL
+  AND c.YearQuarterID >= 'B012'
+  ORDER BY c.YearQuarterID
+--*/
 GO
+
 GRANT SELECT ON  [dbo].[vw_Class] TO [WebApplicationUser]
 GO
