c7da29482de2e22c84e33fe9f99a9e938d80c0ab
Shawn South <shawn.south@bellevuecollege.edu>
2012-03-07 18:37:05 -0800

    added documentaiton

--- CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
+++ CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
-//      Build Number
+//      Minor Version - increment for Production releases
+//      Build Number - increment for Test/QA releases
 //      Revision
 //
 // You can specify all the values or you can default the Revision and Build Numbers
