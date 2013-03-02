704e924a9259234bfebcba3f8706ed3b35d707de
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-01 16:55:42 -0800

    Added a new model class for the QuarterNavMenu
    partial view.

---
+++ CTCODS/CTCClassSchedule/Models/QuarterNavigationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ctc.Ods.Types;
+
+namespace CTCClassSchedule.Models
+{
+	public class QuarterNavigationModel
+	{
+		public YearQuarter CurrentQuarter { get; set; }
+		public IList<YearQuarter> NavigationQuarters { get; set; }
+	}
+}
