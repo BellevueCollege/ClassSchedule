e84e93228f76c890aa786dcfb75ea1e35e33a9e6
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-01 16:51:58 -0800

    new model class for the Subject view. This should
    allow us to refactor out the majority of ViewBag variables.

---
+++ CTCODS/CTCClassSchedule/Models/SubjectViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ctc.Ods.Types;
+
+namespace CTCClassSchedule.Models
+{
+	public class SubjectViewModel
+	{
+		public string Slug { get; set; }
+		public string SubjectTitle { get; set; }
+		public string SubjectIntro { get; set; }
+		public string DepartmentTitle { get; set; }
+		public string DepartmentURL { get; set; }
+		public YearQuarter CurrentQuarter { get; set; }
+		public IEnumerable<Course> Courses { get; set; }
+		public IList<YearQuarter> NavigationQuarters { get; set; }
+	}
+}
