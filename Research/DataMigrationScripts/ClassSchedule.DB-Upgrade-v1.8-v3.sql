e8303370a9cd3c6afdeec568acb2f9d7b8d4e6c0
Shawn South <shawn.south@bellevuecollege.edu>
2013-03-05 12:28:32 -0800

    Added script (unmodified) generated from Red
    Gate's SQL Compare tool.

--- Research/DataMigrationScripts/ClassSchedule.DB-Upgrade-v1.8-v3.sql
+++ Research/DataMigrationScripts/ClassSchedule.DB-Upgrade-v1.8-v3.sql
@@ -63,7 +63,457 @@ GO
               **                   !! IT MAY EVEN BREAK YOUR DATABASE !!                 **
               *****************************************************************************
  -----------------------------------------------------------------------------------------------------------*/
+/*
+Run this script on:
 
+        MSSQL-D01\TestMSSQL2008.ClassSchedule (Rev. 1118)    -  This database will be modified
+
+to synchronize it with:
+
+        MSSQL-D01\DevMSSQL2008.ClassSchedule (Rev. 1118)
+
+You are recommended to back up your database before running this script
+
+Script created by SQL Compare version 10.3.1 from Red Gate Software Ltd at 3/5/2013 12:25:23 PM
+
+*/
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+BEGIN TRANSACTION
+GO
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Altering [dbo].[vw_Class]'
+GO
+
+
+ALTER view [dbo].[vw_Class]
+AS
+SELECT c.[ClassID]
+      ,c.[YearQuarterID]
+      ,c.[ItemNumber]
+      ,c.[CourseID]
+      ,c.[Department]
+      ,c.[CourseNumber]
+-- NOTE: The capacity/enrollment logic only works for B013 (Spring 2011) and later.
+-- Previous to that, associated data in the cluster table is NULL. - shawn.south@bellevuecollege.edu
+      ,CASE
+		WHEN NOT c.[ClusterItemNumber] IS NULL
+			THEN l.ClusterCapacity
+			ELSE c.[ClassCapacity]
+		END AS ClassCapacity
+      ,CASE
+		WHEN NOT c.[ClusterItemNumber] IS NULL
+			THEN l.ClusterEnrolled
+			ELSE c.[StudentsEnrolled]
+		END AS StudentsEnrolled
+	  ,u.LastUpdated
+/* COMMENT THIS LINE FOR DEBUGGING
+	  ,c.ClusterItemNumber AS "(ClusterItemNumber)"
+	  ,c.ClassCapacity AS "(ClassCapacity)"
+	  ,c.StudentsEnrolled AS "(StudentsEnrolled)"
+	  ,l.ClusterCapacity AS "(ClusterCapacity)"
+	  ,l.ClusterEnrolled AS "(ClusterEnrolled)"
+--*/
+  FROM [ODS].[dbo].[vw_Class] c
+	LEFT JOIN [ODS].[dbo].[vw_ClassCluster] l ON l.ClusterItemNumber = c.ClusterItemNumber AND l.YearQuarterID = c.YearQuarterID
+/* COMMENT THIS LINE FOR DEBUGGING
+  WHERE NOT c.ClusterItemNumber IS NULL
+  AND c.YearQuarterID >= 'B012'
+  ORDER BY c.YearQuarterID
+--*/
+	LEFT OUTER JOIN (
+		SELECT TOP (1)
+			UpdateDate AS LastUpdated
+		FROM ODS.dbo.vw_TableTransferLog
+		WHERE (ODSTableName = 'Class')
+		ORDER BY LastUpdated DESC
+	) AS u ON 1 = 1
+
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SectionsMeta]'
+GO
+CREATE TABLE [dbo].[SectionsMeta]
+(
+[ClassID] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Footnote] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Title] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Description] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_Sections] on [dbo].[SectionsMeta]'
+GO
+ALTER TABLE [dbo].[SectionsMeta] ADD CONSTRAINT [PK_Sections] PRIMARY KEY CLUSTERED  ([ClassID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SectionSeats]'
+GO
+CREATE TABLE [dbo].[SectionSeats]
+(
+[ClassID] [char] (8) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SeatsAvailable] [int] NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_SeatAvailibility] on [dbo].[SectionSeats]'
+GO
+ALTER TABLE [dbo].[SectionSeats] ADD CONSTRAINT [PK_SeatAvailibility] PRIMARY KEY CLUSTERED  ([ClassID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[CourseMeta]'
+GO
+CREATE TABLE [dbo].[CourseMeta]
+(
+[CourseID] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Footnote] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_Courses] on [dbo].[CourseMeta]'
+GO
+ALTER TABLE [dbo].[CourseMeta] ADD CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED  ([CourseID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Altering [dbo].[vw_ClassScheduleData]'
+GO
+
+
+ALTER VIEW [dbo].[vw_ClassScheduleData]
+AS
+SELECT
+	c.ClassID
+	,c.YearQuarterID
+	,c.CourseID
+	,CASE
+		WHEN isNULL(c.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(s.LastUpdated, DATEADD(day, - 2, getdate()))
+			THEN CASE
+				WHEN c.ClassCapacity - c.StudentsEnrolled < 0
+					THEN 0
+				ELSE c.ClassCapacity - c.StudentsEnrolled
+			END
+		ELSE s.SeatsAvailable
+	 END AS SeatsAvailable
+	,CASE
+		WHEN isNULL(c.LastUpdated, DateAdd(day, - 1, getdate())) > isNULL(s.LastUpdated, DATEADD(day, - 2, getdate()))
+			THEN c.LastUpdated
+		ELSE s.LastUpdated
+	 END AS LastUpdated
+	,cm.Footnote AS CourseFootnote
+	,sm.Footnote AS SectionFootnote
+	--,p.Abbreviation
+	--,p.ProgramURL
+	--,p.Title
+	--,p.URL
+	--,p.Division
+	--,p.ContactName
+	--,p.ContactPhone
+	--,p.Intro
+	,sm.Title
+	,sm.Description
+FROM dbo.vw_Class c
+LEFT OUTER JOIN dbo.SectionSeats s ON c.ClassID = s.ClassID
+LEFT OUTER JOIN dbo.SectionsMeta sm ON c.ClassID = sm.ClassID
+LEFT OUTER JOIN dbo.CourseMeta cm ON c.CourseID = cm.CourseID
+--LEFT OUTER JOIN dbo.ProgramInformation AS p ON c.Department = p.Abbreviation
+
+
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[Divisions]'
+GO
+CREATE TABLE [dbo].[Divisions]
+(
+[DivisionID] [int] NOT NULL IDENTITY(1, 1),
+[Title] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[URL] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_Divisions] on [dbo].[Divisions]'
+GO
+ALTER TABLE [dbo].[Divisions] ADD CONSTRAINT [PK_Divisions] PRIMARY KEY CLUSTERED  ([DivisionID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[Departments]'
+GO
+CREATE TABLE [dbo].[Departments]
+(
+[DepartmentID] [int] NOT NULL IDENTITY(1, 1),
+[DivisionID] [int] NULL,
+[Title] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[URL] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ProgramChairSID] [varchar] (9) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_Departments] on [dbo].[Departments]'
+GO
+ALTER TABLE [dbo].[Departments] ADD CONSTRAINT [PK_Departments] PRIMARY KEY CLUSTERED  ([DepartmentID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[Subjects]'
+GO
+CREATE TABLE [dbo].[Subjects]
+(
+[SubjectID] [int] NOT NULL IDENTITY(1, 1),
+[DepartmentID] [int] NULL,
+[Title] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Intro] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Slug] [varchar] (63) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[LastUpdatedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastUpdated] [datetime] NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_Subjects] on [dbo].[Subjects]'
+GO
+ALTER TABLE [dbo].[Subjects] ADD CONSTRAINT [PK_Subjects] PRIMARY KEY CLUSTERED  ([SubjectID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SubjectsCoursePrefixes]'
+GO
+CREATE TABLE [dbo].[SubjectsCoursePrefixes]
+(
+[SubjectID] [int] NOT NULL,
+[CoursePrefixID] [varchar] (5) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_SubjectsCoursePrefixes] on [dbo].[SubjectsCoursePrefixes]'
+GO
+ALTER TABLE [dbo].[SubjectsCoursePrefixes] ADD CONSTRAINT [PK_SubjectsCoursePrefixes] PRIMARY KEY CLUSTERED  ([SubjectID], [CoursePrefixID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SectionCourseCrosslistings]'
+GO
+CREATE TABLE [dbo].[SectionCourseCrosslistings]
+(
+[ClassID] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CourseID] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_CourseSections] on [dbo].[SectionCourseCrosslistings]'
+GO
+ALTER TABLE [dbo].[SectionCourseCrosslistings] ADD CONSTRAINT [PK_CourseSections] PRIMARY KEY CLUSTERED  ([ClassID], [CourseID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding constraints to [dbo].[Subjects]'
+GO
+ALTER TABLE [dbo].[Subjects] ADD CONSTRAINT [UQ_Subjects_Slug] UNIQUE NONCLUSTERED  ([Slug])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding foreign keys to [dbo].[Subjects]'
+GO
+ALTER TABLE [dbo].[Subjects] ADD CONSTRAINT [FK_Subjects_Departments] FOREIGN KEY ([DepartmentID]) REFERENCES [dbo].[Departments] ([DepartmentID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding foreign keys to [dbo].[Departments]'
+GO
+ALTER TABLE [dbo].[Departments] ADD CONSTRAINT [FK_Departments_Division] FOREIGN KEY ([DivisionID]) REFERENCES [dbo].[Divisions] ([DivisionID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding foreign keys to [dbo].[SubjectsCoursePrefixes]'
+GO
+ALTER TABLE [dbo].[SubjectsCoursePrefixes] ADD CONSTRAINT [FK_SubjectsCoursePrefixes_Subjects] FOREIGN KEY ([SubjectID]) REFERENCES [dbo].[Subjects] ([SubjectID])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Altering permissions on [dbo].[ClassSearch]'
+GO
+GRANT SELECT ON  [dbo].[ClassSearch] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[CourseMeta]'
+GO
+GRANT SELECT ON  [dbo].[CourseMeta] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[CourseMeta] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[CourseMeta] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[CourseSearch]'
+GO
+GRANT SELECT ON  [dbo].[CourseSearch] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[Departments]'
+GO
+GRANT SELECT ON  [dbo].[Departments] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[Departments] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[Departments] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[Divisions]'
+GO
+GRANT SELECT ON  [dbo].[Divisions] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[Divisions] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[Divisions] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[SectionCourseCrosslistings]'
+GO
+GRANT SELECT ON  [dbo].[SectionCourseCrosslistings] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[SectionCourseCrosslistings] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[SectionCourseCrosslistings] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[SectionSeats]'
+GO
+GRANT SELECT ON  [dbo].[SectionSeats] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[SectionSeats] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[SectionSeats] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[SectionsMeta]'
+GO
+GRANT SELECT ON  [dbo].[SectionsMeta] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[SectionsMeta] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[SectionsMeta] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[Subjects]'
+GO
+GRANT SELECT ON  [dbo].[Subjects] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[Subjects] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[Subjects] TO [WebApplicationUser]
+GO
+PRINT N'Altering permissions on [dbo].[SubjectsCoursePrefixes]'
+GO
+GRANT SELECT ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
+GRANT INSERT ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
+GRANT UPDATE ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
+GO
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The database update succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The database update failed'
+GO
+DROP TABLE #tmpErrors
+GO
+/*
+Start of RedGate SQL Source Control versioning database-level extended properties.
+*/
+DECLARE @RG_SC_VERSION BIGINT
+SET @RG_SC_VERSION = 1118
+IF EXISTS (SELECT 1 FROM fn_listextendedproperty(N'SQLSourceControl Database Revision', NULL, NULL, NULL, NULL, NULL, NULL))
+  EXEC sp_dropextendedproperty N'SQLSourceControl Database Revision', NULL, NULL, NULL, NULL, NULL, NULL
+EXEC sp_addextendedproperty N'SQLSourceControl Database Revision', @RG_SC_VERSION, NULL, NULL, NULL, NULL, NULL, NULL
+GO
+IF EXISTS (SELECT 1 FROM fn_listextendedproperty(N'SQLSourceControl Scripts Location', NULL, NULL, NULL, NULL, NULL, NULL))
+  EXEC sp_dropextendedproperty N'SQLSourceControl Scripts Location', NULL, NULL, NULL, NULL, NULL, NULL
+EXEC sp_addextendedproperty N'SQLSourceControl Scripts Location',  N'<?xml version="1.0" encoding="utf-16" standalone="yes"?>
+<ISOCCompareLocation version="1" type="TfsLocation">
+  <ServerUrl>https://tfsvs.wacollabcoop.org/tfs/bellevuecollege</ServerUrl>
+  <SourceControlFolder>$/ODSAPI/ODSAPI/CTCODS/CTCClassSchedule.DB</SourceControlFolder>
+</ISOCCompareLocation>', NULL, NULL, NULL, NULL, NULL, NULL
+GO
+IF EXISTS (SELECT 1 FROM fn_listextendedproperty(N'SQLSourceControl Migration Scripts Location', NULL, NULL, NULL, NULL, NULL, NULL))
+  EXEC sp_dropextendedproperty N'SQLSourceControl Migration Scripts Location', NULL, NULL, NULL, NULL, NULL, NULL
+EXEC sp_addextendedproperty N'SQLSourceControl Migration Scripts Location',  N'<?xml version="1.0" encoding="utf-16" standalone="yes"?>
+<ISOCCompareLocation version="1" type="TfsLocation">
+  <ServerUrl>https://tfsvs.wacollabcoop.org/tfs/bellevuecollege</ServerUrl>
+  <SourceControlFolder>$/ODSAPI/ODSAPI/CTCODS/CTCClassSchedule.DB.MigrationScripts</SourceControlFolder>
+</ISOCCompareLocation>', NULL, NULL, NULL, NULL, NULL, NULL
+GO
+/*
+End of RedGate SQL Source Control versioning database-level extended properties.
+*/
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) IF EXISTS(SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The database update succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The database update failed'
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+-- End Red Gate SQL Compare-generated script
 
 
 /*-----------------------------------------------------------------------------------------------------------
