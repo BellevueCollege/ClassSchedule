d681b56e49ce0bb47dea13bc31dff337d75cd548
Shawn South <shawn.south@bellevuecollege.edu>
2013-03-06 23:59:55 -0800

    Incremented version for release

--- CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
+++ CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.8.5.5")]
-[assembly: AssemblyFileVersion("1.8.5.5")]
+[assembly: AssemblyVersion("2.9.0.0")]
+[assembly: AssemblyFileVersion("2.9.0.0")]
