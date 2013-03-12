08fd001904169c8246a7e234d881fc96661bbd7b
Shawn South <shawn.south@bellevuecollege.edu>
2013-03-12 16:56:02 -0700

    Added BoolExtensions, which will need to
    eventually be moved into the CtcApi.

---
+++ CTCODS/CTCClassSchedule/Common/BoolExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace CtcApi.Extensions
+{
+  public static class BoolExtensions
+  {
+    public static bool InclusiveOr(this bool bln, bool expression)
+    {
+// ReSharper disable RedundantAssignment
+      bln = (bln || expression);
+// ReSharper restore RedundantAssignment
+      return expression;
+    }
+  }
+}
