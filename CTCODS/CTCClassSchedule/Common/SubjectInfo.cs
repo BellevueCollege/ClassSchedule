7c5385e53cca3c9d54b2fa29675876886175d014
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-12 17:07:43 -0700

    Update SubjectInfo.cs
    (1) Refactored the GetSubjectInfo() method to avoid several
        NullReferenceException pitfalls.
    
    (2) Added a new method which accepts a course prefix, and returns the
        Subject which owns it.

--- CTCODS/CTCClassSchedule/Common/SubjectInfo.cs
+++ CTCODS/CTCClassSchedule/Common/SubjectInfo.cs
@@ -7,9 +7,6 @@ using System.Text.RegularExpressions;
 
 namespace CTCClassSchedule.Common
 {
-	// TODO: Add GetSubjectsFromPrefix methods accepting either a CoursePrefix, SubjectCoursePrefix, ScheduleCoursePrefix, or String?
-	//		   Should return a list of Subjects which are using the specified Prefix
-
 	/// <summary>
 	/// Static helper class to return data related to a given subject.
 	/// </summary>
@@ -44,6 +41,36 @@ namespace CTCClassSchedule.Common
 			return subject;
 		}
 
+    /// <summary>
+    /// Takes a course <paramref name="prefix"/> parameter and attempts to return a single matching <see cref="Subject"/> entity.
+    /// </summary>
+    /// <remarks>This method is not absolutely secure. The database schema says that prefixes-to-subjects is a many-to-many relationship,
+    /// so theoretically a single <paramref name="prefix"/> could return one or more <see cref="Subject"/>s. This makes it impossible to
+    /// determine which <see cref="Subject"/>s a course belongs to based solely on course prefixes. However, realistically no course prefix
+    /// is (currently) merged with more than a single <see cref="Subject"/>. Therefore, we can abuse the fact that in reality, there is
+    /// effectively a many-to-one relationship between prefixes and <see cref="Subject"/>s.
+    /// TODO: In the future we may want to modify the database schema to make this many-to-one relationship explicit rather than implied.</remarks>
+    /// <param name="prefix"></param>
+    /// <returns>The matching <see cref="Subject"/> entity, or null if no match was found</returns>
+    /// <exception cref="Exception">Throws a general exception if more than one subject is matched to the specified <paramref name="prefix"/></exception>
+    public static Subject GetSubjectFromPrefix(string prefix)
+    {
+      Subject subject = null;
+      using (ClassScheduleDb db = new ClassScheduleDb())
+      {
+        IList<Subject> matches = db.SubjectsCoursePrefixes.Where(s => s.CoursePrefixID.Equals(prefix, StringComparison.OrdinalIgnoreCase))
+                                                          .Select(s => s.Subject).ToList();
+        if (matches.Count > 1)
+        {
+          string subjectList = matches.Select(s => s.Title).Aggregate((i, j) => i + ", " + j);
+          throw new Exception(String.Concat("The course prefix ", prefix, " belongs to more than one subject: ", subjectList, "."));
+        }
+        subject = matches.FirstOrDefault();
+      }
+
+      return subject;
+    }
+
 		/// <summary>
 		/// Gathers a <see cref="Subject"/>'s related <see cref="Department"/>, <see cref="Division"/>, and <see cref="SubjectsCoursePrefix"/>s
 		/// </summary>
@@ -60,23 +87,21 @@ namespace CTCClassSchedule.Common
 					return result;
 				}
 
-				result = new SubjectInfoResult();
-				result.Subject = subject;
-				result.SubjectCoursePrefixes = subject.SubjectsCoursePrefixes.ToList();
-				result.Department = subject.Department;
+        result = new SubjectInfoResult
+        {
+          Subject = subject,
+          SubjectCoursePrefixes = subject.SubjectsCoursePrefixes.ToList(),
+          Department = subject.Department ?? new Department(),
+        };
+        result.Division = result.Department.Division ?? new Division();
 
-				if (result.Department != null)
-				{
-					// If the url is a fully qualified url (e.g. http://continuinged.bellevuecollege.edu/about)
-					// or empty just return it, otherwise prepend with the current school url.
-					string deptUrl = result.Department.URL ?? string.Empty;
-					if (!string.IsNullOrWhiteSpace(deptUrl) && !Regex.IsMatch(deptUrl, @"^https?://"))
-					{
-						result.Department.URL = deptUrl;
-					}
-
-					result.Division = result.Department.Division;
-				}
+        // If the url is a fully qualified url (e.g. http://continuinged.bellevuecollege.edu/about)
+        // or empty just return it, otherwise prepend with the current school url.
+        string deptUrl = result.Department.URL ?? string.Empty;
+        if (!string.IsNullOrWhiteSpace(deptUrl) && !Regex.IsMatch(deptUrl, @"^https?://"))
+        {
+          result.Department.URL = deptUrl;
+        }
 			}
 
 			return result;
