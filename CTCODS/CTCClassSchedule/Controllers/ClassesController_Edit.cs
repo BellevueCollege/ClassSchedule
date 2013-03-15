d5a75fa0ef2d4f61a10a3d3c4844231fcd321f05
Shawn South <shawn.south@bellevuecollege.edu>
2013-03-15 15:17:14 -0700

    Refactored SectionEdit functionality to work with
    the new database schema, and implemented ability to add and delete
    cross-listed CourseIDs.

--- CTCODS/CTCClassSchedule/Controllers/ClassesController_Edit.cs
+++ CTCODS/CTCClassSchedule/Controllers/ClassesController_Edit.cs
@@ -5,18 +5,21 @@ using System.Data.Objects;
 using System.Data.Objects.DataClasses;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Mvc;
 using CTCClassSchedule.Models;
 using Common.Logging;
 using Ctc.Ods;
 using Ctc.Ods.Data;
 using Ctc.Ods.Types;
+using CtcApi.Extensions;
 using CtcApi.Web.Mvc;
 using System.Web.Security;
 using DotNetCasClient;
 using CTCClassSchedule.Common;
-using Microsoft.Security.Application;
+using Encoder = Microsoft.Security.Application.Encoder;
 
 namespace CTCClassSchedule.Controllers
 {
@@ -112,7 +115,7 @@ namespace CTCClassSchedule.Controllers
 	                SectionWithSeats section = sectionsEnum.First();
 	                string secID = section.ID.ToString();
 
-	                /*SectionEditModel sectionModel = new SectionEditModel
+	                SectionEditModel sectionModel = new SectionEditModel
 	                                                  {
 	                                                    SectionToEdit = section,
 	                                                    CrossListedCourses = db.SectionCourseCrosslistings.Where(x => x.ClassID == secID)
@@ -121,8 +124,7 @@ namespace CTCClassSchedule.Controllers
 	                                                                           .ToList()
 	                                                  };
 
-	                return PartialView(sectionModel);*/
-
+	                return PartialView(sectionModel);
 	              }
 	              _log.Warn(m => m("Cannot Edit Section '{0}' - no records found.", sectionID));
 	              return PartialView(null);
@@ -147,54 +149,133 @@ namespace CTCClassSchedule.Controllers
 	  /// <summary>
 	  /// Saves the updated <see cref="Section"/> information
 	  /// </summary>
-	  /// <param name="collection"></param>
+	  /// <param name="Model"></param>
+	  /// <param name="sectionID"></param>
+	  /// <param name="CrossListedCourses"></param>
 	  /// <returns></returns>
 	  [HttpPost]
-    // BUG: I'm concerned that form input is not being validated by the following line
-    // It looks like this was added to work around common issues with passing FormCollection objects in MVC3,
-    // but I'm sure there is a better way to handle this - such that input from the form is being validated.
-    //  - shawn.south@bellevuecollege.edu, 3/7/2013
-	  [ValidateInput(false)]
 	  [AuthorizeFromConfig(RoleKey = "ApplicationEditor")]
-	  public ActionResult SectionEdit(FormCollection collection)
+    public ActionResult SectionEdit([ModelBinder(typeof(SectionEditModelBinder))]SectionEditModel Model, string sectionID, ICollection<string> CrossListedCourses)
 	  {
-
-/* TODO: Refactor saving Section information
- *
- *  1.  Receive a collection of course crosslistings (see Api/ProgramEdit - can we just pass the collection,
- *      or do we need to pass the Model too?
- *
- *  2.  If needed, refactor other SectionEdit to pass a SectionMeta to the view.
- *
- *  3.  Remove ValidateInputAttribute once we've replaced FormCollection parameter.
- */
-	    string referrer = collection["referrer"];
+      string referrer = (Request.UrlReferrer != null ? Request.UrlReferrer.ToString() : string.Empty);
 
 	    if (HttpContext.User.Identity.IsAuthenticated)
 	    {
-	      string itemNumber = collection["ItemNumber"];
-	      string yrq = collection["Yrq"];
-	      string username = HttpContext.User.Identity.Name;
-	      string sectionFootnotes = StripHtml(collection["section.SectionFootnotes"]);
-	      string classID = itemNumber + yrq;
-	      string customTitle = collection["section.CustomTitle"];
-	      string customDescription = StripHtml(collection["section.CustomDescription"]);
-
 	      if (ModelState.IsValid)
 	      {
 	        using (ClassScheduleDb db = new ClassScheduleDb())
 	        {
 	          try
 	          {
-	            SectionsMeta itemToUpdate = GetItemToUpdate(db.SectionsMetas, s => s.ClassID == classID);
+              string username = HttpContext.User.Identity.Name;
 
-	            itemToUpdate.ClassID = classID;
-	            itemToUpdate.Footnote = sectionFootnotes;
-	            itemToUpdate.LastUpdated = DateTime.Now;
-	            itemToUpdate.LastUpdatedBy = username;
-	            itemToUpdate.Title = customTitle == string.Empty ? null : customTitle;
-	            itemToUpdate.Description = customDescription;
+              // HACK: sectionID value is explicitly/separately passed to this Action method.
+              // The nested custom ModelBinders (see ModelBinderAttribute in method signature) seem to work, but
+              // the ID value still comes through as NULL. When there is more time I'd like to continue trying
+              // to get the ModelBinders to work properly.
+              //  - shawn.south@bellevuecollege.edu, 3/14/2013
+              IQueryable<SectionsMeta> sects = db.SectionsMetas.Where(m => m.ClassID == sectionID);
+	            int sectCount = sects.Count();
 
+              SectionsMeta sectionMeta;
+
+	            if (sectCount > 0)
+	            {
+	              if (sectCount == 1)
+	              {
+	                sectionMeta = sects.Single();
+	              }
+	              else
+	              {
+	                _log.Warn(m => m("More than one SectionsMeta record found for '{0}'. Using the first and ignoring others."));
+	                sectionMeta = sects.First();
+	              }
+
+                // update values, if they've changed
+	              bool changed = false;
+
+                // TODO: Strip HTML from input before assigning
+                if (TestAndUpdate(Model.SectionToEdit.CustomTitle != sectionMeta.Title, ref changed))
+                {
+	                sectionMeta.Title = Model.SectionToEdit.CustomTitle;
+	              }
+                if (TestAndUpdate(Model.SectionToEdit.CustomDescription != sectionMeta.Description, ref changed))
+	              {
+                  sectionMeta.Description = Model.SectionToEdit.CustomDescription;
+	              }
+                if (TestAndUpdate(Model.SectionToEdit.SectionFootnotes != sectionMeta.Footnote, ref changed))
+                {
+                  sectionMeta.Footnote = Model.SectionToEdit.SectionFootnotes;
+	              }
+
+	              if (changed)
+	              {
+	                sectionMeta.LastUpdated = DateTime.Now;
+	                sectionMeta.LastUpdatedBy = username;
+	              }
+	            }
+	            else
+	            {
+	              sectionMeta = new SectionsMeta
+	                               {
+	                                 ClassID = sectionID,
+	                                 Title = Model.SectionToEdit.CustomTitle,
+	                                 Description = Model.SectionToEdit.CustomDescription,
+	                                 Footnote = Model.SectionToEdit.SectionFootnotes,
+	                                 LastUpdated = DateTime.Now,
+                                   LastUpdatedBy = username
+	                               };
+
+                db.SectionsMetas.AddObject(sectionMeta);
+	            }
+
+              IEnumerable<SectionCourseCrosslisting> currentSectionCrosslistings = db.SectionCourseCrosslistings.Where(x => x.ClassID == sectionID);
+              int currentCrosslistCount = currentSectionCrosslistings.Count();
+
+	            if (CrossListedCourses != null && CrossListedCourses.Count > 0)
+	            {
+                // remove existing cross-listings from the database they weren't passed
+                if (currentCrosslistCount > 0)
+	              {
+	                IList<SectionCourseCrosslisting> unmergables = currentSectionCrosslistings.Where(x => !CrossListedCourses.Contains(x.CourseID)).ToList();
+	                foreach (SectionCourseCrosslisting course in unmergables)
+	                {
+	                  db.SectionCourseCrosslistings.DeleteObject(course);
+	                }
+	              }
+
+                // add any new cross-listings
+	              IEnumerable<string> crosslistingsToAdd = CrossListedCourses; // start with the full list...
+	              if (currentCrosslistCount > 0)
+	              {
+                  // ...then, if we already have records in the db, replace with just those items that aren't there yet
+	                crosslistingsToAdd = CrossListedCourses.Where(c => !currentSectionCrosslistings.Select(x => x.CourseID).Contains(c));
+	              }
+
+                foreach (string prefix in crosslistingsToAdd)
+                {
+                  db.SectionCourseCrosslistings.AddObject(new SectionCourseCrosslisting
+                  {
+                    ClassID = sectionID,
+                    CourseID = prefix
+                  });
+
+                }
+              }
+	            else
+	            {
+                // Since no CrossListedCourses were passed, delete all existing ones from the database
+	              if (currentCrosslistCount > 0)
+	              {
+                  // TODO: There's got to be a better way to delete multiple records than looping through them.
+	                foreach (SectionCourseCrosslisting crosslistRecord in currentSectionCrosslistings)
+	                {
+	                  db.SectionCourseCrosslistings.DeleteObject(crosslistRecord);
+	                }
+	              }
+	            }
+
+              // saves ALL additions/deletions/changes made earlier in this method
 	            db.SaveChanges();
 	          }
 	          catch (Exception ex)
@@ -205,7 +286,17 @@ namespace CTCClassSchedule.Controllers
 	      }
 	      else
 	      {
-	        _log.Warn(m => m("Section changes NOT saved - ModelState is not Valid."));
+          // Collect all ModelError messages...
+          StringBuilder msg = new StringBuilder();
+          foreach (string key in ModelState.Keys)
+          {
+            foreach (ModelError err in ModelState[key].Errors)
+            {
+              // ...so we can display them.
+              msg.AppendFormat("[{0}] - {1}\n", key, string.IsNullOrWhiteSpace(err.ErrorMessage) ? err.Exception.ToString() : err.ErrorMessage);
+            }
+          }
+	        _log.Warn(m => m("Section changes NOT saved - ModelState is not Valid.\n{0}", msg));
 	      }
 	    }
       else
@@ -220,6 +311,27 @@ namespace CTCClassSchedule.Controllers
 
 	  #region Private methods
 	  /// <summary>
+	  /// Tests <paramref name="expression"/> and updates a <paramref name="mutable"/> status flag
+	  /// </summary>
+	  /// <param name="expression"></param>
+	  /// <param name="mutable">
+	  ///   A flag which maintains the status of <paramref name="expression"/> inclusively OR'd with its previous value.
+	  ///   THE VALUE OF <paramref name="mutable"/> MAY CHANGE.
+	  /// </param>
+	  /// <remarks>
+    ///   This method supports performing multiple successive Boolean tests while keeping track of if <i>any</i> of them
+    ///   were true. Especially useful for determining if changes were made to any of a collection of data fields.
+    /// </remarks>
+	  /// <returns>
+	  ///   The Boolean evaluation of <paramref name="expression"/>
+	  /// </returns>
+	  private static bool TestAndUpdate(bool expression, ref bool mutable)
+    {
+      mutable = (mutable || expression);
+      return expression;
+    }
+
+    /// <summary>
 	  ///
 	  /// </summary>
 	  /// <typeparam name="T"></typeparam>
