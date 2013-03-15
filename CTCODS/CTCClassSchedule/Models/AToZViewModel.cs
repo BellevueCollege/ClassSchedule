28277ebe6f543ade3034a96504f9111d351d8e26
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-15 15:03:18 -0700

    Added strongly typed models for the AllClasses and
    a_to_z views.

---
+++ CTCODS/CTCClassSchedule/Models/AToZViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CTCClassSchedule.Models
+{
+  public class AToZViewModel
+  {
+    public IList<char> LettersList { get; set; }
+    public char? ViewingLetter { get; set; }
+  }
+}
---
+++ CTCODS/CTCClassSchedule/Models/AllClassesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ctc.Ods.Types;
+
+namespace CTCClassSchedule.Models
+{
+  public class AllClassesModel
+  {
+    public YearQuarter CurrentQuarter { get; set; }
+    public IList<Subject> Subjects { get; set; }
+    public IList<YearQuarter> NavigationQuarters { get; set; }
+    public IList<char> LettersList { get; set; }
+    public char? ViewingLetter { get; set; }
+  }
+}
