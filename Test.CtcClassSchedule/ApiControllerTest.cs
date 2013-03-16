5e3e0ef5c34602b351357a227723a694e4eabd54
Shawn South <shawn.south@bellevuecollege.edu>
2013-03-15 17:51:44 -0700

    Added unit test framework for forthcoming calls to
    ApiController.

---
+++ Test.CtcClassSchedule/ApiControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using CTCClassSchedule.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using System.Web.Mvc;
+
+namespace Test.CtcClassSchedule
+{
+
+
+    /// <summary>
+    ///This is a test class for ApiControllerTest and is intended
+    ///to contain all ApiControllerTest Unit Tests
+    ///</summary>
+  [TestClass()]
+  public class ApiControllerTest
+  {
+
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+
+    [TestMethod()]
+    public void CrossLinkedCoursesTest()
+    {
+      //ApiController target = new ApiController();
+      //string SectionID = "3003B234";
+      //JsonResult actual = target.CrossLinkedCourses(SectionID);
+
+      //Assert.IsNotNull(actual);
+
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+  }
+}
