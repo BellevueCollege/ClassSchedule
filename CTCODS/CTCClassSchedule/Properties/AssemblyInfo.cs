25abb826a439af9dc1c5131e06674a29687f1945
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-19 18:35:18 -0700

    Incremented the version number to 3.0.1.0

--- CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
+++ CTCODS/CTCClassSchedule/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.0.4")]
-[assembly: AssemblyFileVersion("2.9.0.4")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
