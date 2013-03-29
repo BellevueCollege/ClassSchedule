2f06272a1f527a71f346bfb018310071950bec5c
Andrew Craswell <andrew.craswell@bellevuecollege.edu>
2013-03-29 13:22:42 -0700

    Added code documentation in the form of summary
    comments. All methods and properties have been documented.

--- CTCODS/CTCClassSchedule/Common/ClassScheduleExporter.cs
+++ CTCODS/CTCClassSchedule/Common/ClassScheduleExporter.cs
@@ -7,15 +7,28 @@ using System.Web.Mvc;
 using System.Text;
 using Ctc.Ods.Types;
 using Ctc.Ods.Data;
+using CTCClassSchedule.Controllers;
 
 namespace CTCClassSchedule.Common
 {
+  // A short alias for the data structure used to group Class Schedule data logically
   using GroupedClassScheduleData = Dictionary<Division, Dictionary<Subject, IList<SectionsBlock>>>;
-  using CTCClassSchedule.Controllers;
 
+  /// <summary>
+  /// Static class to manage exporting all Class Schedule data to an Adobe InDesign format
+  /// which is used for the print version of the Class Schedule.
+  /// </summary>
   public static class ClassScheduleExporter
   {
 
+    /// <summary>
+    /// Manages the collection of all CLass Schedule data, and outputs it as a file.
+    /// </summary>
+    /// <remarks>
+    /// This is the only public method in the class. It serves as a bootstrap for the export process.
+    /// </remarks>
+    /// <param name="yearQuarter">The quarter which is being exported.</param>
+    /// <returns>A FileResult with a text/plain UTF8 encoded file as an HTML response.</returns>
     public static FileResult GetFile(YearQuarter yearQuarter)
     {
       // Convert each object to a node
@@ -36,13 +49,21 @@ namespace CTCClassSchedule.Common
     }
 
 
+
+    /// <summary>
+    /// Gets a grouping of all Class Schedule data to export, and converts them to a series of <see cref="IExportableNode"/>s
+    /// which each contain the logic necessary for converting the contained data (<see cref="Division"/>, <see cref="Section"/>, etc)
+    /// to a string format.
+    /// </summary>
+    /// <param name="yearQuarter">The quarter which is being exported.</param>
+    /// <returns>A list of <see cref="IExportableNode"/>s representing Class Schedule data, in the proper sorted order.</returns>
     private static IList<IExportableNode> GetClassScheduleDataNodes(YearQuarter yearQuarter)
     {
       IList<IExportableNode> nodes = new List<IExportableNode>();
       GroupedClassScheduleData divisionSections = GroupClassScheduleData(yearQuarter);
 
 
-      // Convert all Division, Departments, Subjects, and Sections into nodes
+      // Convert all Division, Subjects, and Sections into exportable nodes
       // Sort each group of data as we convert it to nodes
       foreach (Division div in divisionSections.Keys.OrderBy(d => d.Title)) // Divisions
       {
@@ -59,6 +80,14 @@ namespace CTCClassSchedule.Common
 
       return nodes;
     }
+
+    /// <summary>
+    /// Pulls all the Class Schedule data for the given <paramref name="yearQuarter"/>, and groups it into
+    /// a logically ordered complex data structure, which allows us to easilyu traverse all the data in
+    /// logical order.
+    /// </summary>
+    /// <param name="yearQuarter">The quarter which is being grouped.</param>
+    /// <returns></returns>
     private static GroupedClassScheduleData GroupClassScheduleData(YearQuarter yearQuarter)
     {
       GroupedClassScheduleData divisionSectionsBlock = new GroupedClassScheduleData();
@@ -101,6 +130,7 @@ namespace CTCClassSchedule.Common
           sectionsWithSeats = Helpers.GetSectionsWithSeats(yearQuarter.ID, sections, db);
         }
 
+        // A final LINQ query which converts the returned SectionsWithSeats to the final complex data structure
         divisionSectionsBlock = (from d in subjects.Where(r => sectionsWithSeats.Any(q => r.CoursePrefixes.Any(p => p.CoursePrefixID == q.CourseSubject)))
                                                    .Select(s => s.Department.Division)
                                                    .Distinct()
@@ -108,8 +138,9 @@ namespace CTCClassSchedule.Common
                                    d => d,
                                    s => s.Departments.SelectMany(de => de.Subjects)
                                                      .Where(r => sectionsWithSeats.Any(q => r.CoursePrefixes.Any(p => p.CoursePrefixID == q.CourseSubject)))
-                                                     .ToDictionary(k => k, v => Helpers.groupSectionsIntoBlocks(sectionsWithSeats.Where(q => v.CoursePrefixes.Any(p => q.CourseSubject == p.CoursePrefixID)).ToList(), db))
-                                );
+                                                     .ToDictionary(k => k,
+                                                                   v => Helpers.groupSectionsIntoBlocks(sectionsWithSeats.Where(q => v.CoursePrefixes.Any(p => q.CourseSubject == p.CoursePrefixID)).ToList(), db))
+                                 );
 
       }
 
@@ -118,35 +149,90 @@ namespace CTCClassSchedule.Common
 
 
 
+
+
+    /// <summary>
+    /// This interface is empty, and only serves the purpose of allowing all
+    /// nodes of different types to be added to the same collect, so long as they
+    /// implement this interface.
+    ///
+    /// It is not necessary to expose any functionality, since the ToString()
+    /// method is used to output the nodes contents. This method inherits from
+    /// <see cref="Object"/> regardless.
+    /// </summary>
     private interface IExportableNode
     {
     }
+
+    /// <summary>
+    /// A generic node type which implements shared functionality which all nodes require.
+    /// This generic allows any data type (ex: <see cref="Subject"/>, <see cref="Section"/>, etc)
+    /// to be stored in a node, and hooks up the ToString() method to output a string representation of
+    /// the data. The function which actually creates the string representation must be overriden
+    /// and implemented in all child classes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     private abstract class ExportableNode<T> : IExportableNode
     {
+      /// <summary>
+      /// Used to build the string representation of the Node.
+      /// </summary>
       protected StringBuilder builder = new StringBuilder();
-      protected T Node;
 
+      /// <summary>
+      /// A generic Node, holding whatever type of data needs to be exported.
+      /// </summary>
+      protected T Node { get; set; }
+
+      /// <summary>
+      /// Constructor which enforces an object is passed, and stored in the Node property.
+      /// </summary>
+      /// <param name="node"></param>
       public ExportableNode(T node)
       {
         Node = node;
       }
 
+      /// <summary>
+      /// An abstract method which should be overriden in child classes and used
+      /// to implement the logic of converting the Node to it's string representation.
+      /// </summary>
+      /// <returns>A string representation of the Node.</returns>
       protected abstract string ExportNodeAsString();
 
-      public override string ToString()
+      /// <summary>
+      /// Basic ToString override which returns the string representation of the Node.
+      /// The method is sealed so that inheriting classes cannot override this behavior.
+      /// </summary>
+      /// <returns></returns>
+      public sealed override string ToString()
       {
         return ExportNodeAsString();
       }
     }
 
+    /// <summary>
+    /// An exportable node class which holds a <see cref="Subject"/> and contains
+    /// all the necessary logic and rules for outputting a text representation of it
+    /// for file exportation.
+    /// </summary>
     private class ExportableSubjectNode : ExportableNode<Subject>
     {
-
+      /// <summary>
+      /// Default constructor which passes the <see cref="Subject"/> data
+      /// to the base constructor to be stored in the base Node.
+      /// </summary>
+      /// <param name="node">The <see cref="Subject"/> to store in the base Node.</param>
       public ExportableSubjectNode(Subject node)
         : base(node)
       {
       }
 
+      /// <summary>
+      /// Contains the logic used to convert the <see cref="Subject"/> Node into
+      /// a string representation for export.
+      /// </summary>
+      /// <returns>A string representation of the <see cref="Subject"/> Node.</returns>
       protected override string ExportNodeAsString()
       {
         int subjectSeparatedLineBreaks = 4;
@@ -162,15 +248,33 @@ namespace CTCClassSchedule.Common
         return builder.ToString();
       }
     }
+
+    /// <summary>
+    /// An exportable node class which holds a <see cref="SectionsBlock"/> and contains
+    /// all the necessary logic and rules for outputting a text representation of it
+    /// for file exportation.
+    ///
+    /// The logic contatined is complex, and outputs all sections, linked sections,
+    /// aggregates common footnotes, and also aggregates automated hybrid footnotes where possible/
+    /// </summary>
     private class ExportableSectionsBlockNode : ExportableNode<SectionsBlock>
     {
 
+      /// <summary>
+      /// Default constructor which passes the <see cref="SectionsBlock"/> data
+      /// to the base constructor to be stored in the base Node.
+      /// </summary>
+      /// <param name="node">The <see cref="SectionsBlock"/> to store in the base Node.</param>
       public ExportableSectionsBlockNode(SectionsBlock node)
         : base(node)
       {
       }
 
-
+      /// <summary>
+      /// Contains the logic used to convert the <see cref="SectionsBlock"/> Node into
+      /// a string representation for export.
+      /// </summary>
+      /// <returns>A string representation of the <see cref="SectionsBlock"/> Node.</returns>
       protected override string ExportNodeAsString()
       {
         SectionWithSeats primarySection = Node.Sections.First();
@@ -250,6 +354,11 @@ namespace CTCClassSchedule.Common
 
 
       #region Private Helpers
+      /// <summary>
+      /// Takes a <see cref="SectionWithSeats"/> and constructs the header string representation.
+      /// </summary>
+      /// <param name="section">The <see cref="SectionWithSeats"/> used to construct the header.</param>
+      /// <returns>A string representation of the sections header.</returns>
       private static string getSectionExportHeader(SectionWithSeats section)
       {
         string creditsText = section.Credits.ToString();
