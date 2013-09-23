a80045d315af13000ba2f65dd3813984e6f8db1e
Shawn South <shawn.south@bellevuecollege.edu>
2013-09-23 12:46:04 -0700

    Incremented version for QA release, updated
    customError settings for 404 errors to match changes made in QA and
    synced database files.

--- CTCODS/CTCClassSchedule.DB/Tables/dbo.SubjectsCoursePrefixes.sql
+++ CTCODS/CTCClassSchedule.DB/Tables/dbo.SubjectsCoursePrefixes.sql
@@ -3,11 +3,12 @@ CREATE TABLE [dbo].[SubjectsCoursePrefixes]
 [CoursePrefixID] [varchar] (5) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SubjectID] [int] NOT NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[SubjectsCoursePrefixes] WITH NOCHECK ADD
+CONSTRAINT [FK_SubjectsCoursePrefixes_Subjects] FOREIGN KEY ([SubjectID]) REFERENCES [dbo].[Subjects] ([SubjectID])
 GO
 ALTER TABLE [dbo].[SubjectsCoursePrefixes] ADD CONSTRAINT [PK_SubjectsCoursePrefixes_1] PRIMARY KEY CLUSTERED  ([CoursePrefixID]) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[SubjectsCoursePrefixes] ADD CONSTRAINT [FK_SubjectsCoursePrefixes_Subjects] FOREIGN KEY ([SubjectID]) REFERENCES [dbo].[Subjects] ([SubjectID])
-GO
+
 GRANT SELECT ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
 GRANT INSERT ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
 GRANT DELETE ON  [dbo].[SubjectsCoursePrefixes] TO [WebApplicationUser]
