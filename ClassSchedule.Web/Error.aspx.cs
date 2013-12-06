d7347eed2813fc23cb0d26ee90ca2215804f7bb3
Shawn South <shawn.south@bellevuecollege.edu>
2013-12-05 18:38:41 -0800

    Added license information

--- ClassSchedule.Web/Error.aspx.cs
+++ ClassSchedule.Web/Error.aspx.cs
@@ -1,6 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿/*
+This file is part of CtcClassSchedule.
+
+CtcClassSchedule is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CtcClassSchedule is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CtcClassSchedule.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
