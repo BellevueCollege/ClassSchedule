7690c489d1534c35972091e7fb37b643c6491f13
Shawn South <shawn.south@bellevuecollege.edu>
2014-02-24 11:53:56 -0800

    Fixed error on backslash in search string.
    FogBugz:3312
    Primary problem was that a partial view was retrieving the searchterm
    from the query string instead of the sanitized one from the ViewBag.
    
    Also cleaned up redirects when not enough information (e.g. search
    term, quarter) was provided to Search action.

--- ClassSchedule.Web/Controllers/SearchController.cs
+++ ClassSchedule.Web/Controllers/SearchController.cs
@@ -26,6 +26,7 @@ using Ctc.Ods.Data;
 using Ctc.Ods.Types;
 using CTCClassSchedule.Common;
 using CTCClassSchedule.Models;
+using Microsoft.Security.Application;
 using MvcMiniProfiler;
 
 namespace CTCClassSchedule.Controllers
@@ -44,25 +45,27 @@ namespace CTCClassSchedule.Controllers
 		// GET: /Search/
 		public ActionResult Index(string searchterm, string Subject, string quarter, string timestart, string timeend, string day_su, string day_m, string day_t, string day_w, string day_th, string day_f, string day_s, string f_oncampus, string f_online, string f_hybrid, string avail, string latestart, string numcredits, int p_offset = 0)
 		{
-		  if (string.IsNullOrWhiteSpace(searchterm))
-		  {
-        // display home page
-		    return RedirectToAction("Index", "Classes");
-		  }
+      if (String.IsNullOrWhiteSpace(searchterm))
+      {
+        if (!string.IsNullOrWhiteSpace(quarter))
+        {
+          return RedirectToAction("YearQuarter", "Classes", new {YearQuarter = quarter});
+        }
+        return RedirectToAction("Index", "Classes");
+      }
 
-			// We don't currently support quoted phrases. - 4/19/2012, shawn.south@bellevuecollege.edu
-			searchterm = searchterm.Replace("\"", string.Empty);
+		  // We don't currently support quoted phrases. - 4/19/2012, shawn.south@bellevuecollege.edu
+      // Also, remove any backslashes
+		  searchterm = searchterm.Replace("\"", string.Empty).Replace(@"\", string.Empty);
+		  
+      // protect against injection attacks
+		  searchterm = Encoder.UrlEncode(searchterm);
 
-			// TODO: This needs to be configurable
+		  // TODO: This needs to be configurable
 			if (quarter == "CE")
 			{
 				Response.Redirect("http://www.campusce.net/BC/Search/Search.aspx?q=" + searchterm, true);
 				return null;
-			}
-
-			if (String.IsNullOrEmpty(searchterm.Trim()))
-			{
-				return RedirectToAction("AllClasses", "Classes", new { YearQuarterID = quarter });
 			}
 
       // TODO: replace ViewBag calls w/ ref to FacetHelper
